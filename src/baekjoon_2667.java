66f24eccc5def6817bac04dbdd1525f5ba91f3d0
tkv00 <144890194+tkv00@users.noreply.github.com>
2024-05-31 21:54:35 +0900

    5월 31일

---
+++ src/baekjoon_2667.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class baekjoon_10026 {
+    public static int N;
+    public static void main(String[] args) {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br, br.readLine());
+
+    }
+}
