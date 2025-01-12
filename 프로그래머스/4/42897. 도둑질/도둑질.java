6fc09cb4bb56dad836e0faead651ce834f011216
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-01-12 19:15:07 +0900

    [level 4] Title: 도둑질, Time: 40.23 ms, Memory: 103 MB -BaekjoonHub

--- 프로그래머스/4/42897. 도둑질/도둑질.java
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -1,23 +1,23 @@
 class Solution {
+    private static int[] dp1;
+    private static int[] dp2;
+    
     public int solution(int[] money) {
-        int size=money.length;
-        int[] dp1=new int[size];
-        int[] dp2=new int[size];
-        
-        //첫 번째 집을 포함하는 경우
+        dp1=new int[money.length];
+        dp2=new int[money.length];
+       //첫 번째 집 선택o -> 마지막 집 선택 불가
         dp1[0]=money[0];
         dp1[1]=money[0];
+      //첫 번째 집 선택x -> 마지막 집 선택 가능
+        dp2[1]=money[1];
         
-        for(int i=2;i<size-1;i++){
+        for(int i=2;i<money.length-1;i++){
+            //직전 집 선택o,직전 집 선택x
             dp1[i]=Math.max(dp1[i-1],dp1[i-2]+money[i]);
         }
-        
-        
-        //첫 번째 집을 포함하지 않는 경우
-        dp2[1]=money[1];
-        for(int i=2;i<size;i++){
+        for(int i=2;i<money.length;i++){
             dp2[i]=Math.max(dp2[i-1],dp2[i-2]+money[i]);
         }
-        return Math.max(dp1[size-2],dp2[size-1]);
+        return Math.max(dp1[money.length-2],dp2[money.length-1]);
     }
 }
