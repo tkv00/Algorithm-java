d91f116f4d5af859f4f814d70c4e28eabdf1425e
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-01-12 21:21:16 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 55.23 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,33 +1,33 @@
-import java.util.HashMap;
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        //이름-추천인
-        HashMap<String,String> name_recommand=new HashMap<>();
-        //이름-받는 비용
-        HashMap<String,Integer> name_cost=new HashMap<>();
+        int[]result=new int[enroll.length];
+        //사람-금액 해시
+        HashMap<String,Integer> person_money=new HashMap<>();
+        //사람-부모 해시
+        HashMap<String,String> person_parent=new HashMap<>();
         
+        //초기화
         for(int i=0;i<enroll.length;i++){
-            name_recommand.put(enroll[i],referral[i]);
+            person_money.put(enroll[i],0);
+            person_parent.put(enroll[i],referral[i]);
         }
-        
         
         for(int i=0;i<seller.length;i++){
+            String now=seller[i];
             int cost=amount[i]*100;
-            String curName=seller[i];
-            
-            while(cost>0 && curName!="-"){
-                name_cost.put(curName,name_cost.getOrDefault(curName,0)+cost-(cost/10));
-                //다음 이름으로 넘어가기
-                cost/=10;
-                curName=name_recommand.get(curName);
+            while(!now.equals("-")){
+                person_money.put(now,person_money.getOrDefault(now,0)+cost-cost/10);
+                now=person_parent.get(now);
+                cost=cost/10;
+                if(cost==0)break;
             }
-            
         }
-        
-        int[] answer=new int[enroll.length];
-        for(int i=0;i<answer.length;i++){
-            answer[i]=name_cost.getOrDefault(enroll[i],0);
+      
+        for(int i=0;i<enroll.length;i++){
+            result[i]=person_money.get(enroll[i]);
         }
-        return answer;
+        return result;
     }
 }
