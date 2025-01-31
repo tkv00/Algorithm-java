1d3fb33cc49ca057e38b031bc6406973aacf3d2b
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-01-31 15:50:02 +0900

    [level 3] Title: 베스트앨범, Time: 7.53 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,40 +2,28 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String,Integer> map=new HashMap<>();
-        
-        //장르별 가지고 있는 인덱스 맵
-        //장르- 인덱스,재생횟수
-        HashMap<String,HashMap<Integer,Integer>>indexMap=new HashMap<>();
-        ArrayList<Integer>answer=new ArrayList<>();
+        List<Integer>answer=new ArrayList<>();
+        HashMap<String,Integer> map1=new HashMap<>();
+        HashMap<String,List<Integer>> map2=new HashMap<>();
         
         for(int i=0;i<genres.length;i++){
-            //키값이 존재하지 않는 경우
-            if(!map.containsKey(genres[i])){
-                HashMap<Integer,Integer>hash=new HashMap<>();
-                hash.put(i,plays[i]);
-                map.put(genres[i],plays[i]);
-                indexMap.put(genres[i],hash);
-            }else{
-                map.put(genres[i],map.get(genres[i])+plays[i]);
-                indexMap.get(genres[i]).put(i,plays[i]);
+            map1.put(genres[i],map1.getOrDefault(genres[i],0)+plays[i]);
+            if(!map2.containsKey(genres[i])){
+                map2.put(genres[i],new ArrayList<>());
             }
+            //인덱스 저장
+            map2.get(genres[i]).add(i);
         }
         
-        ArrayList<String>keyMap=new ArrayList<>(map.keySet());
-       
-        //정렬
-        Collections.sort(keyMap,(o1,o2)->map.get(o2).compareTo(map.get(o1)));
-      
-        for(String key:keyMap){
-            HashMap<Integer,Integer>music=indexMap.get(key);
-            ArrayList<Integer> genre_key=new ArrayList<>(music.keySet());
-            Collections.sort(genre_key,(o1,o2)->music.get(o2).compareTo(music.get(o1)));
+        List<String> keySet=new ArrayList<>(map1.keySet());
+        keySet.sort((o1,o2)->map1.get(o2).compareTo(map1.get(o1)));
+        
+        for(String str:keySet){
+            List<Integer> arr=map2.get(str);
+            arr.sort((o1,o2)->plays[o2]-plays[o1]);
             
-            //일단 1개 넣기
-            answer.add(genre_key.get(0));
-            if(genre_key.size()>1){
-                answer.add(genre_key.get(1));
+            for(int i=0;i<Math.min(2,arr.size());i++){
+                answer.add(arr.get(i));
             }
         }
         return answer.stream().mapToInt(Integer::intValue).toArray();
