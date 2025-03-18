cfc6cbcc8ff8bcac410b3e69debf15eb17113c39
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-03-18 23:44:27 +0900

    [level 2] Title: 방문 길이, Time: 18.98 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,42 +1,96 @@
 import java.util.*;
-
+class Point{
+    int x;
+    int y;
+    
+    Point(int x,int y){
+        this.x=x;
+        this.y=y;
+    }
+}
 class Solution {
+    private static int[] dx={0,-1,0,1};
+    private static int[] dy={-1,0,1,0};
+    private static boolean[][]visited;
     
     //적합성 판단
     private static boolean isValid(int x,int y){
-        if(x<0  || x>=11 || y<0 || y>=11)return false;
+        if(x<1 || x>=12 || y<1 || y>=12)return false;
         return true;
     }
-    
-    private static final HashMap<Character,int[]> location=new HashMap<>();
-    
-    private static void initLocation(){
-        location.put('U',new int[]{0,1});
-        location.put('D',new int[]{0,-1});
-        location.put('L',new int[]{-1,0});
-        location.put('R',new int[]{1,0});
-    }
     public int solution(String dirs) {
-        initLocation();
-        int x=5,y=5;
-        HashSet<String> answer=new HashSet<>();
+        int size=dirs.length();
+        char[] array=dirs.toCharArray();
+        int result=0;
+        //11*11
+        visited=new boolean[12][12];
+        //(0,0) 방문처리
+        visited[5][5]=true;
         
-        for(int i=0;i<dirs.length();i++){
-            int[] offset=location.get(dirs.charAt(i));
-            int nx=x+offset[0];
-            int ny=y+offset[1];
-            if(!isValid(nx,ny)){
-                continue;
+        Deque<Point> dq=new ArrayDeque<>();
+        dq.addFirst(new Point(5,5));
+        
+        for(int i=0;i<size;i++){
+            Point now=dq.pollLast();
+            int now_x=now.x;
+            int now_y=now.y;
+            
+            switch(array[i]){
+                case 'L':
+                    int new_x=now_x+dx[0];
+                    int new_y=now_y+dy[0];
+                    if(!isValid(new_x,new_y)) continue;
+                    
+                    //방문 안 한 경우
+                    if(!visited[new_x][new_y]){
+                        visited[new_x][new_y]=true;
+                        result++;
+                    }
+                    dq.addFirst(new Point(new_x,new_y));
+                    break;
+                    
+                case 'U':
+                    int new_x=now_x+dx[1];
+                    int new_y=now_y+dy[1];
+                    if(!isValid(new_x,new_y)) continue;
+                    
+                    //방문 안 한 경우
+                    if(!visited[new_x][new_y]){
+                        visited[new_x][new_y]=true;
+                        result++;
+                    }
+                    dq.addFirst(new Point(new_x,new_y));
+                    break;
+                    
+                 case 'R':
+                    int new_x=now_x+dx[2];
+                    int new_y=now_y+dy[2];
+                    if(!isValid(new_x,new_y)) continue;
+                    
+                    //방문 안 한 경우
+                    if(!visited[new_x][new_y]){
+                        visited[new_x][new_y]=true;
+                        result++;
+                    }
+                    dq.addFirst(new Point(new_x,new_y));
+                    break;
+                
+                 case 'D':
+                    int new_x=now_x+dx[3];
+                    int new_y=now_y+dy[3];
+                    if(!isValid(new_x,new_y)) continue;
+                    
+                    //방문 안 한 경우
+                    if(!visited[new_x][new_y]){
+                        visited[new_x][new_y]=true;
+                        result++;
+                    }
+                    dq.addFirst(new Point(new_x,new_y));
+                    break;
+                    
             }
-            
-            answer.add(x+" "+y+" "+nx+" "+ny);
-            answer.add(nx+" "+ny+" "+x+" "+y);
-            
-            x=nx;
-            y=ny;
-            
         }
-        return answer.size()/2;
+        return result;
         
     }
 }
