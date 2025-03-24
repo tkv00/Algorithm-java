d3b4715086f757a604392910369794669a5696e0
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-03-24 18:44:27 +0900

    [level 2] Title: 튜플, Time: 87.92 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.java
+++ 프로그래머스/2/64065. 튜플/튜플.java
@@ -2,25 +2,19 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        //순서가 다르면 -> 다른 튜플
-        String str=s.substring(2,s.length()-2).replace("{","");
+        String []str=s.substring(2,s.length()-2).split("\\},\\{");
+        Arrays.sort(str,Comparator.comparingInt(String::length));
+        Set<Integer> set=new LinkedHashSet<>();
         
-        String[] arr=str.split("},");
-        int[] result=new int[arr.length];
         
-        Arrays.sort(arr,(o1,o2)->Integer.compare(o1.length(),o2.length()));
-        HashSet<String> set=new HashSet<>();
-        
-        for(int i=0;i<arr.length;i++){
-            String[] strArray=arr[i].split(",");
-            for(int j=0;j<strArray.length;j++){
-                //set에 없으면 정답 배열에 추가
-                if(!set.contains(strArray[j])){
-                    result[i]=Integer.parseInt(strArray[j]);
-                    set.add(strArray[j]);
-                }
+        for(String st:str){
+            String[]ns=st.split(",");
+            for(String k:ns){
+                set.add(Integer.parseInt(k));
             }
+            
         }
-        return result;
+        
+        return set.stream().mapToInt(Integer::intValue).toArray();
     }
 }
