7f92edaf894dab2f8df16df7782f0c67b443a9ed
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-11 17:26:05 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,9 +1,14 @@
--- 코드를 입력하세요
-SELECT ANIMAL_ID, NAME ,
-            CASE WHEN SEX_UPON_INTAKE LIKE 'Neutered%' OR SEX_UPON_INTAKE LIKE 'Spayed%' THEN 'O' 
-            ELSE 'X' 
-            END 
-            AS 중성화
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID ASC;
-            
+select
+    animal_id,
+    name,
+    case
+        when sex_upon_intake like 'Neutered%' then 'O'
+        when sex_upon_intake like 'Spayed%' then 'O'
+        else 'X'
+        end
+    as '중성화'
+
+from
+    animal_ins
+order by
+    animal_id;
