6f9d037dd0f93d0b9b63dbe88e081ca84d074f35
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-17 03:58:14 +0900

    [level 3] Title: 물고기 종류 별 대어 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
+++ 프로그래머스/3/293261. 물고기 종류 별 대어 찾기/물고기 종류 별 대어 찾기.sql
@@ -1,13 +1,16 @@
--- 1. 물고기 종류별 가장 큰 물고기
--- 2. 물고기 ID 오름차순
--- 3. 가장 큰 물고기 10cm이면 포함x
-SELECT I.ID,N.FISH_NAME,I.LENGTH
-FROM FISH_INFO AS I
-     INNER JOIN FISH_NAME_INFO AS N ON I.FISH_TYPE = N.FISH_TYPE
-WHERE LENGTH >= 10 AND
-      (I.FISH_TYPE,I.LENGTH) IN (
-        SELECT FISH_TYPE,MAX(LENGTH)
-        FROM FISH_INFO
-        GROUP BY FISH_TYPE
-      )
-ORDER BY I.ID ASC;
+select
+    a.id,
+    b.fish_name,
+    a.length
+from
+    fish_info as a
+    inner join
+    fish_name_info as b
+on
+    a.fish_type = b.fish_type
+where
+    a.length = (
+        select max(length)
+        from fish_info as ft
+        where ft.fish_type = a.fish_type
+    );
