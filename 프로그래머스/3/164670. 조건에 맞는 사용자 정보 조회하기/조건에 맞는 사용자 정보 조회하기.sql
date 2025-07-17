2d26f973e17d88603506ccab28e6d9481088ae54
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-17 22:59:04 +0900

    [level 3] Title: 조건에 맞는 사용자 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
+++ 프로그래머스/3/164670. 조건에 맞는 사용자 정보 조회하기/조건에 맞는 사용자 정보 조회하기.sql
@@ -1,16 +1,16 @@
--- 코드를 입력하세요
--- 1.중고거래 게시물 3건 이상
--- 2.아이디,닉네임,전체 주소,전화번호
--- 3. 전체 주소 : 시+도로명 주소+상세주소
--- 4. 전화번호 -삽입
--- 5. 회원 아이디 내림차순
-
-SELECT U.USER_ID,
-       U.NICKNAME,
-       CONCAT(U.CITY,' ',U.STREET_ADDRESS1,' ',U.STREET_ADDRESS2) AS 전체주소,
-       CONCAT(SUBSTR(U.TLNO,1,3),'-',SUBSTR(U.TLNO,4,4),'-',SUBSTR(U.TLNO,8,4)) AS 전화번호
-FROM USED_GOODS_USER AS U
-    INNER JOIN USED_GOODS_BOARD AS B ON U.USER_ID = B.WRITER_ID
-GROUP BY B.WRITER_ID
-HAVING COUNT(B.WRITER_ID) >= 3
-ORDER BY U.USER_ID DESC;
+select
+    u.user_id,
+    u.nickname,
+    concat(u.city," ",u.street_address1," ",u.street_address2) as '전체주소',
+    concat(substring(u.tlno,1,3),"-",substring(u.tlno,4,4),"-",substring(u.tlno,8,4)) as '전화번호'
+from
+    used_goods_board as b
+    inner join
+    used_goods_user as u
+    on b.writer_id = u.user_id
+group by
+    b.writer_id
+having
+    count(b.board_id)>=3
+order by
+    u.user_id desc;
