d34d97f82a05894bddefae9f87b5b43862191f85
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-18 18:50:45 +0900

    [Gold IV] Title: 플로이드, Time: 368 ms, Memory: 42644 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,61 +1,48 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.StringTokenizer;
-
+import java.util.*;
 public class Main {
-    public static int n;
-    public static int m;
-    public static StringTokenizer st;
-    public static int[][] graph;
-    public static final int MAX_COST = 10000001;
+    private static int N,M;
+    private static int[][] map;
+    private static StringTokenizer st;
+    private static final int INF=1000000001;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br.readLine());
+        M=Integer.parseInt(br.readLine());
+        map=new int[N+1][N+1];
+        for(int i=1;i<=N;i++){
+            Arrays.fill(map[i],INF);
+            map[i][i]=0;
+        }
+        for(int i=0;i<M;i++){
+            st=new StringTokenizer(br.readLine());
+            int start=Integer.parseInt(st.nextToken());
+            int end=Integer.parseInt(st.nextToken());
+            int cost=Integer.parseInt(st.nextToken());
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        m = Integer.parseInt(br.readLine());
-        graph = new int[n + 1][n + 1];
-        for (int i = 1; i <= n; i++) {
-            Arrays.fill(graph[i], MAX_COST);
+            map[start][end]=Math.min(map[start][end],cost);
         }
 
-        for (int i = 1; i <= m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            graph[start][end] = Math.min(graph[start][end], cost);
-
-        }
-        //System.out.println(Arrays.deepToString(graph));
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= n; j++) {
-                if (i == j) continue;
-                for (int k = 1; k <= n; k++) {
-                    if (i == k || k == j) continue;
-                    graph[j][k] = Math.min(graph[j][k], graph[j][i] + graph[i][k]);
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                for(int k=1;k<=N;k++){
+                    map[j][k]=Math.min(map[j][k],map[j][i]+map[i][k]);
                 }
             }
         }
-        StringBuilder sb = new StringBuilder();
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (graph[i][j] == MAX_COST) {
-                    sb.append("0 ");
-                } else {
-                    sb.append(graph[i][j] + " ");
+        StringBuilder sb=new StringBuilder();
+               for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(map[i][j]==INF){
+                    sb.append(0).append(" ");
+                }else{
+                    sb.append(map[i][j]).append(" ");
                 }
-
             }
             sb.append("\n");
         }
-        System.out.println(sb);
 
-
+        System.out.print(sb);
     }
-
 }
