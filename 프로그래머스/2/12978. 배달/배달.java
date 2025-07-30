4f15c2759d68cdab83e28f7fe15087f97cc03a25
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-31 05:53:58 +0900

    [level 2] Title: 배달, Time: 12.30 ms, Memory: 91 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,55 +1,39 @@
 import java.util.*;
 
 class Solution {
-    private static class Node{
-    int idx;
-    int cost;
-    Node(int idx,int cost){
-        this.idx=idx;
-        this.cost=cost;
-        }
-    }
-    private static int answer=0;
+    private static int[][] map;
+    private static final int INF=Integer.MAX_VALUE;
     public int solution(int N, int[][] road, int K) {
-        ArrayList<Node>[] map=new ArrayList[N+1];
-        for(int i=0;i<=N;i++){
-            map[i]=new ArrayList<>();
-        }
-        for(int[] r:road){
-            int start=r[0];
-            int end=r[1];
-            int cost=r[2];
-            
-            //양방향 설정
-            map[start].add(new Node(end,cost));
-            map[end].add(new Node(start,cost));
+        map=new int[N+1][N+1];
+        int cnt=0;
+        for(int i=1;i<=N;i++){
+            Arrays.fill(map[i],INF);
         }
         
-        int[] dist=new int[N+1];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-            
-        PriorityQueue<Node> pq=new PriorityQueue<>((o1,o2)->Integer.compare(o1.cost,o2.cost));
-        pq.add(new Node(1,0));
-        dist[1]=0;
-            
-        while(!pq.isEmpty()){
-            Node now=pq.poll();
+        for(int i=1;i<=N;i++){
+            map[i][i]=0;
+        }
+        for(int[] info:road){
+            int row=info[0];
+            int col=info[1];
+            map[row][col]=Math.min(map[row][col],info[2]);
+            map[col][row]=Math.min(map[col][row],info[2]);
+        }
+        
+        for(int k=1;k<=N;k++){
+            for(int i=1;i<=N;i++){
+                for(int j=1;j<=N;j++){
+                    if(i==j || i==k || j==k) continue;
+                    if(map[i][k]!=INF && map[k][j]!=INF){
+                        map[i][j]=Math.min(map[i][j],map[i][k]+map[k][j]);        
+                    }
                 
-            if(now.cost>dist[now.idx]) continue;
-                
-            for(Node next:map[now.idx]){
-                if(now.cost+next.cost<dist[next.idx]){
-                    dist[next.idx]=now.cost+next.cost;
-                    pq.add(new Node(next.idx,dist[next.idx]));
                 }
             }
         }
-        
-         for(int i=1;i<=N;i++){
-                if(dist[i]<=K){
-                    answer++;
-                }
-            }
-        return answer;             
+        for(int i=1;i<=N;i++){
+            if(map[1][i]<=K)cnt++;
+        }
+        return cnt;
     }
 }
