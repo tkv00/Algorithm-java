dcad9a89df028e26d45e7d5ca8b3805e118ab65a
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-31 05:31:36 +0900

    [level 3] Title: 표 편집, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/81303. 표 편집/표 편집.java
+++ 프로그래머스/3/81303. 표 편집/표 편집.java
@@ -1,46 +1,97 @@
 import java.util.*;
 
 class Solution {
-    public String solution(int n, int k, String[] cmd) {
-        char[] result=new char[n];
-        for(int i=0;i<n;i++){
-            result[i]='O';
+    private static final char UP='U';
+    private static final char DOWN='D';
+    private static final char DELETE='C';
+    private static final char RESTORE='Z';
+    private static class Node{
+        Node prev=null;
+        Node next=null;
+        boolean deleted;
+        Node moveDown(int cnt){
+            Node now=this;
+            for(int i=0;i<cnt;i++){
+                now=now.next;
+            }
+            return now;
         }
-        int[] up=new int[n+2];
-        int[] down=new int[n+2];
-        
-        for(int i=0;i<n+2;i++){
-            up[i]=i-1;
-            down[i]=i+1;
+        Node moveUp(int cnt){
+            Node now=this;
+            for(int i=0;i<cnt;i++){
+                now=now.prev;
+            }
+            return now;
         }
         
-        //삭제 저장
-        Stack<Integer> stack=new Stack<>();
-        int idx=k;
-        idx++;
-        for(String str:cmd){
-            if(str.startsWith("C")){
-                up[down[idx]]=up[idx];
-                down[up[idx]]=down[idx];
-                stack.push(idx);
-                
-                idx=n<down[idx] ? up[idx] : down[idx];
-            }else if(str.startsWith("Z")){
-                int restore=stack.pop();
-                down[up[restore]]=restore;
-                up[down[restore]]=restore;
-            }else{
-                String[] nstr=str.split(" ");
-                int x=Integer.parseInt(nstr[1]);
-                for(int i=0;i<x;i++){
-                    idx=nstr[0].equals("U") ? up[idx] : down[idx];
-                }
+        void restore(){
+            this.deleted=false;
+            Node prev=this.prev;
+            Node next=this.next;
+            
+            if(prev!=null){
+                prev.next=this;
+            }
+            if(next!=null){
+                next.prev=this;
             }
         }
         
-        for(int i:stack){
-            result[i-1]='X';
+        
+        Node delete(){
+            this.deleted=true;
+            Node prev=this.prev;
+            Node next=this.next;
+            
+            if(prev!=null) prev.next=next;
+            if(next!=null){
+                 next.prev=prev;
+                return next;
+            }
+            return prev;
         }
-        return String.valueOf(result);
+        
+    }
+    public String solution(int n, int k, String[] cmd) {
+        Stack<Node> removedNode=new Stack<>();
+        Node[] nodeList=new Node[n];
+        
+        for(int i=0;i<n;i++){
+            nodeList[i]=new Node();
+            if(i>0) {
+                nodeList[i-1].next=nodeList[i];
+                nodeList[i].prev=nodeList[i-1];    
+            }
+        }
+        Node current=nodeList[k];
+        
+        for(String query:cmd){
+            String[] orders=query.split(" ");
+            char order=orders[0].charAt(0);
+            
+            switch(order){
+                case UP:
+                    current=current.moveUp(Integer.parseInt(orders[1]));
+                    break;
+                case DOWN:
+                    current=current.moveDown(Integer.parseInt(orders[1]));
+                    break;
+                case DELETE:
+                    removedNode.push(current);
+                    current=current.delete();
+                    break;
+                case RESTORE:
+                    Node node=removedNode.pop();
+                    node.restore();
+                    break;
+            }
+        }
+        
+        StringBuilder sb=new StringBuilder();
+        for(Node node:nodeList){
+            if(node.deleted) sb.append("X");
+            else sb.append("O");
+        }
+        return sb.toString();
     }
 }
