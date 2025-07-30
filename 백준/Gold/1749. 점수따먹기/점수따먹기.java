59b7187fa94d0c750b4b7e9ebf411b28ba3cca2b
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-07-30 16:00:16 +0900

    [Gold IV] Title: 점수따먹기, Time: 1340 ms, Memory: 21044 KB -BaekjoonHub

--- 백준/Gold/1749. 점수따먹기/점수따먹기.java
+++ 백준/Gold/1749. 점수따먹기/점수따먹기.java
@@ -1,52 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
 public class Main {
-    private static int N;
-    private static int M;
+    private static int max=Integer.MIN_VALUE;
+    private static int N,M;
     private static StringTokenizer st;
     private static int[][] map;
-    private static int[][] arr;
-    private static int max=Integer.MIN_VALUE;
-    private static int getSumOfArray(int row1,int col1,int row2,int col2){
-        return arr[row2][col2]
-                -arr[row1-1][col2]
-                -arr[row2][col1-1]
-                +arr[row1-1][col1-1];
-    }
-    public static void main(String[] args) throws IOException {
+    private static int[][] sum;
+    public static void main(String[] args) throws IOException{
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         st=new StringTokenizer(br.readLine());
-
         N=Integer.parseInt(st.nextToken());
         M=Integer.parseInt(st.nextToken());
-
-        map=new int[N+1][M+1];
-        arr=new int[N+1][M+1];
-
-        for(int i=1;i<=N;i++){
+        map=new int[N][M];
+        sum=new int[N+1][M+1];
+        for(int i=0;i<N;i++){
             st=new StringTokenizer(br.readLine());
-            for(int j=1;j<=M;j++){
+            for(int j=0;j<M;j++){
                 map[i][j]=Integer.parseInt(st.nextToken());
-                //누적합 구하기
-                arr[i][j]=map[i][j]-arr[i-1][j-1]+arr[i-1][j]+arr[i][j-1];
             }
         }
 
+        sum[1][1]=map[0][0];
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=M;j++){
+                sum[i][j]=map[i-1][j-1]+sum[i-1][j]+sum[i][j-1]-sum[i-1][j-1];
+            }
+        }
 
-        for(int row1=1;row1<=N;row1++){
-            for(int col1=1;col1<=M;col1++){
-                for(int row2=row1;row2<=N;row2++){
-                    for(int col2=col1;col2<=M;col2++){
-                        max=Math.max(getSumOfArray(row1,col1,row2,col2),max);
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=M;j++){
+                for(int row=i;row<=N;row++){
+                    for(int col=j;col<=M;col++){
+                        max=Math.max(sum[row][col]-sum[row][j-1]-sum[i-1][col]+sum[i-1][j-1],max);
                     }
                 }
             }
         }
-
         System.out.print(max);
-
     }
 }
