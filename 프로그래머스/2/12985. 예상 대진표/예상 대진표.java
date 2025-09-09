b7cbe5213dded78a870446580a6478aa3ff97ac7
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-09 15:10:13 +0900

    [level 2] Title: 예상 대진표, Time: 0.04 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -1,18 +1,15 @@
+import java.util.*;
 class Solution
 {
-    private static boolean isStop(int i){
-        return i/2==0;
-    }
     public int solution(int n, int a, int b)
     {
+        
         int cnt=0;
-        while(true){
-            a=a/2+a%2;
-            b=b/2+b%2;
+        while(a!=b){
+            a = (a%2==0) ? a/2 : a/2+1;
+            b = (b%2==0) ? b/2 : b/2+1;
             cnt++;
-            if(a==b)break;
         }
-        //1 2 3 4
         return cnt;
     }
 }
