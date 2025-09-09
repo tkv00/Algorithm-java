4bc605d834e656f59749ea264a0f9f6df17d08eb
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-09 16:04:37 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 43.12 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -1,35 +1,61 @@
 import java.util.*;
 
 class Solution {
-    private static int PROFIT=100;
-    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
-        //총 결과
-        HashMap<String,Integer> result = new HashMap<>();
-        //자식-부모 관계 정의
-        HashMap<String,String> child_parent=new HashMap<>();
+    //이익 계산 함수
+    private static int getTotal(int amount){
+        return amount * 100;
+    }
+    private static class Info{
+        String name;
+        int index;
         
+        Info(String name,int index){
+            this.name=name;
+            this.index=index;
+        }
+    }
+    private static Map<String,Integer> map;
+    private static Map<String,Info> tree;
+    private static int[] result;
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        
+        /**
+        1. 추천인에게 10% 배분 
+        2. 추천한 사람한테 10% 내꺼
+        3. 10% 계산한 금액 1원 미만 -> 이득 측정 X , 모두 내꺼
+        **/
+        
+        /**
+        result -> enroll에 등록된 이름으로
+        **/
+        
+        map=new HashMap<>();
+        tree=new HashMap<>();
+        result=new int[enroll.length];
+        
+        //트리 관계 매핑
         for(int i=0;i<enroll.length;i++){
-            result.put(enroll[i],0);
-            child_parent.put(enroll[i],referral[i]);
+            tree.put(enroll[i],new Info(referral[i],i));
         }
         
         for(int i=0;i<seller.length;i++){
-            String child=seller[i];
-            int minus=(PROFIT*amount[i])/10;
-            result.put(child,result.getOrDefault(child,0)+amount[i]*PROFIT-minus);
-            do{
-                child=child_parent.get(child);
-                result.put(child,result.getOrDefault(child,0)+minus-minus/10);
-                minus/=10;
-                if(minus==0) break;
-            }while(!child.equals("-"));
+            int money=getTotal(amount[i]);
+            String name=seller[i];
+            int index=tree.get(name).index;
+            result[index]+=money-(money/10);
+            
+            int left=money/10;
+            while(true){
+                name=tree.get(name).name;
+                if(name.equals("-")){
+                    break;
+                }
+                result[tree.get(name).index]+=left-(left/10);
+                left/=10;
+                if(left==0) break;
+            }
         }
-        int[] resultArray=new int[enroll.length];
-        int idx=0;
-        for(String str:enroll){
-            resultArray[idx++]=result.get(str);
-        }
-        return resultArray;
-        
+        return result;
     }
 }
