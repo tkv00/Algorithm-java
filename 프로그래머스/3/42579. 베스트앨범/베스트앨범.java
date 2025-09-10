f1cc8cbff89a4cf384cb11d34c09ecdb5eca56cd
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-10 14:09:14 +0900

    [level 3] Title: 베스트앨범, Time: 3.31 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,38 +1,54 @@
 import java.util.*;
 
 class Solution {
-    private static final int LIMIT=2;
-    public int[] solution(String[] genres, int[] plays) {
-        //장르-재생 횟수
-        HashMap<String,Integer> genres_count=new HashMap<>();
-        
-        //장르-노래 인덱스
-        HashMap<String,List<Integer>> genres_index=new HashMap<>();
-        
-        int size=genres.length;
-        for(int i=0;i<size;i++){
-            genres_count.put(genres[i],
-                             genres_count.getOrDefault(genres[i],0)+plays[i]);
-            List<Integer> list=genres_index.getOrDefault(genres[i],new ArrayList<>());
-            list.add(i);
-            genres_index.put(genres[i],list);
+    private static class Info{
+        int idx,play;
+        Info(int idx,int play){
+            this.idx=idx;
+            this.play=play;
         }
-        
-        //장르-재생 횟수 map 내림차순 정렬
-        List<String> countKeySet=new ArrayList<>(genres_count.keySet());
-        countKeySet.sort((o1,o2)->genres_count.get(o2)-genres_count.get(o1));
-        
-        List<Integer> result=new ArrayList<>();
-        
-        for(String genre:countKeySet){
-            List<Integer> indexList=genres_index.get(genre);
-            //내림차순 정렬
-            indexList.sort((o1,o2)->plays[o2]-plays[o1]);
-            
-            for(int i=0;i<Math.min(LIMIT,indexList.size());i++){
-                result.add(indexList.get(i));
+    }
+    
+    private static int getSum(List<Info> infos){
+        int sum=0;
+        if(infos.size()>0){
+            for(Info info:infos){
+            sum+=info.play;
             }
         }
-        return result.stream().mapToInt(Integer::intValue).toArray();
+        return sum;
+    }
+    
+    private static TreeMap<String,List<Info>> musicMap;
+    private static final int INF=2;
+        
+    public int[] solution(String[] genres, int[] plays) {
+        List<Integer> resultArr=new ArrayList<>();
+        int size=genres.length;
+        musicMap=new TreeMap<>();
+        
+        for(int i=0;i<size;i++){
+            musicMap.putIfAbsent(genres[i],new ArrayList<>());
+            musicMap.get(genres[i]).add(new Info(i,plays[i]));
+        }
+        
+        List<String> keySet=new ArrayList<>(musicMap.keySet());
+        
+        Collections.sort(keySet,(a,b)->getSum(musicMap.get(b))-getSum(musicMap.get(a)));
+        
+        for(String genre:keySet){
+            List<Info> list=musicMap.get(genre);
+            Collections.sort(list,(a,b)->b.play-a.play);
+            for(int i=0;i<Math.min(list.size(),INF);i++){
+                resultArr.add(list.get(i).idx);
+            }
+        }
+        
+        int[] answer=new int[resultArr.size()];
+        for(int i=0;i<resultArr.size();i++){
+            answer[i]=resultArr.get(i);
+        }
+        
+        return answer;
     }
 }
