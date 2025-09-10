05204ac34747fc4977d3e4211a6fa2a112019a8e
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-10 13:14:50 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 16.11 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 55.5 MB, 시간: 11.00 ms
+메모리: 55.9 MB, 시간: 16.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 28일 23:26:45
+2025년 09월 10일 13:14:47
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,6 +1,48 @@
+import java.util.Queue;
+import java.util.LinkedList;
 class Solution {
-    public int solution(int[][] maps) {
-        int answer = 0;
-        return answer;
+    /**
+    흰 색 : O 검은 색 : X
+    
+    **/
+    private static int[][] dist;
+    private static Queue<int[]> q;
+    private static int endRow=0,endCol=0;
+    private static int[] dx=new int[]{0,0,1,-1};
+    private static int[] dy=new int[]{1,-1,0,0};
+    
+    private static boolean isValid(int row,int col,int R,int C,int[][] map){
+        return row>=0 && row<R && col>=0 && col<C && map[row][col]!=0;
     }
+    
+    public int solution(int[][] maps) {
+        int rowSize=maps.length;
+        int colSize=maps[0].length;
+        
+        endRow=rowSize-1;
+        endCol=colSize-1;
+        
+        dist=new int[rowSize][colSize];
+        q=new LinkedList<>();
+        dist[0][0]=1;
+        q.offer(new int[]{0,0});
+        
+        while(!q.isEmpty()){
+            int[] now=q.poll();
+            for(int i=0;i<4;i++){
+                int newRow=now[0]+dx[i];
+                int newCol=now[1]+dy[i];
+                
+                if(!isValid(newRow,newCol,rowSize,colSize,maps) || dist[newRow][newCol]!=0) continue;
+                
+                dist[newRow][newCol]=dist[now[0]][now[1]]+1;
+                q.offer(new int[]{newRow,newCol});
+            }
+            
+        }
+        
+        return dist[endRow][endCol] == 0 ? -1 : dist[endRow][endCol];
+        
+    }
+    
 }
