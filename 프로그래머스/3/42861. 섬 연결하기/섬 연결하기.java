11e55518a8aab3596add4bf5dc213cd785c0b240
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-11 11:27:49 +0900

    [level 3] Title: 섬 연결하기, Time: 1.22 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,34 +1,37 @@
-import java.util.Arrays;
+import java.util.*;
 class Solution {
-    public int find(int[] arr,int a){
-        if(a==arr[a]) return arr[a];
-        return arr[a]=find(arr,arr[a]);
+    private static int[] arr;
+    private static void union(int a,int b){
+        a=getParent(a);
+        b=getParent(b);
+        if(a>b) arr[a]=b;
+        else arr[b]=a;
     }
-    
-    public void union(int[] arr,int a,int b){
-        a=find(arr,a);
-        b=find(arr,b);
-        if(a<b)arr[b]=a;
-        else arr[a]=b;
+    private static int getParent(int a){
+        if(arr[a]==a) return a;
+        return getParent(arr[a]);
     }
-    
+
+    private static boolean isSameParent(int a,int b){
+        a=getParent(a);
+        b=getParent(b);
+        return a==b;
+    }
     public int solution(int n, int[][] costs) {
-        int[] arr=new int[n];
+        arr=new int[n];
         for(int i=0;i<n;i++){
             arr[i]=i;
         }
-        int totalCost=0;
-        Arrays.sort(costs,(o1,o2)->Integer.compare(o1[2],o2[2]));
-        int bridge=0;
+        Arrays.sort(costs,(a,b)->a[2]-b[2]);
+        int answer=0;
         
-        for(int[] edge:costs){
-            if(bridge==n-1) break;
-            if(find(arr,edge[0])!=find(arr,edge[1])){
-                union(arr,edge[0],edge[1]);
-                totalCost+=edge[2];
-                bridge++;
+        for(int [] cost:costs){
+            if(!isSameParent(cost[0],cost[1])){
+                answer+=cost[2];
+                union(cost[0],cost[1]);
             }
         }
-        return totalCost;
+        
+        return answer;
     }
 }
