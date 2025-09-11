60a4c30b0d45938ec88181a4e2b33ea07e99478b
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-11 13:32:45 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 28.88 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,78 +1,48 @@
 import java.util.*;
-
+import java.io.*;
 class Solution {
-    private static int rowS;
-    private static int colS;
-    private static class Node{
-        int row;
-        int col;
-        Node(int row,int col){
-            this.row=row;
-            this.col=col;
-        }
-    }
-    private static String[] directions={"d","l","r","u"};
-    private static int[] dr={1,0,0,-1};
-    private static int[] dc={0,-1,1,0};
-    //위치 유효성 검사
-    private static boolean isValid(int row,int col){
-        if(row<0 || col<0 || row>=rowS || col>=colS)
-            return false;
-        return true;
+    private static boolean isValid(int nowRow,int nowCol,int row,int col){
+        return nowRow>=0 && nowRow < row && nowCol >= 0 && nowCol<col;
     }
     
-    
-    private static boolean distance(int endRow,int endCol,int row,int col,int k){
-        if(Math.abs(endRow-row)+Math.abs(endCol-col)>k) 
-            return false;
-        return true;
-    }
-    
-    //dfs작성
-    private String dfs(Node node,int cnt,int k,String str,Node end){
-        if(cnt>k) return null;
+    private static final int[] dx=new int[]{1,0,0,-1};
+    private static final int[] dy=new int[]{0,-1,1,0};
+    private static final char[] move=new char[]{'d','l','r','u'};
+    private static int[][] map;
+    private static boolean flag;
+    private static String answer;
+    private static void DFS(int row,int col,int endRow,int endCol,String order,int k,int r,int c){
+        //k값 벗어나는 경우
+        if(flag) return;
+        if(order.length()>k) return;
         
-        if(!distance(end.row,end.col,node.row,node.col,k-cnt)) return null;
-        
-        //남은 횟수로 도달하지 못하는 경우
-        if(cnt==k){
-            //경로 도달
-            if(end.row==node.row && end.col==node.col){
-                return str;
-            }else return null;
+        //도달 못하는 경우
+        int dist=Math.abs(row-endRow)+Math.abs(col-endCol);
+        if(dist>k-order.length()) return;
+        if((k-order.length()-dist)%2!=0) return;
+        //도착한 경우
+        if(row==endRow && col==endCol && order.length()==k){
+            flag=true;
+            answer=order;
+            return;
         }
         
-        
-
         for(int i=0;i<4;i++){
-            int newRow=node.row+dr[i];
-            int newCol=node.col+dc[i];
+            int nextRow=row+dx[i];
+            int nextCol=col+dy[i];
+            String newOrder=order+move[i];
             
-            if(isValid(newRow,newCol)){
-                String result=dfs(new Node(newRow,newCol),cnt+1,k,str+directions[i],end);
-                if(result!=null) return result;
-            }
+            if(!isValid(nextRow,nextCol,r,c)) continue;
+            DFS(nextRow,nextCol,endRow,endCol,newOrder,k,r,c);
         }
-        return null;
     }
-    
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        rowS=n;
-        colS=m;
-        //출발 지점
-        Node startNode=new Node(x-1,y-1);
+        answer="impossible";
+        map=new int[n][m];
+        flag=false;
+     
+        DFS(x-1,y-1,r-1,c-1,"",k,n,m);
         
-        //도착 지점
-        Node endNode=new Node(r-1,c-1);
-        int dist=Math.abs(startNode.row-endNode.row)+Math.abs(startNode.col-endNode.col);
-        
-        if((k-dist)%2!=0 || dist>k) return "impossible";
-        if(!distance(endNode.row,endNode.col,startNode.row,startNode.col,k)){
-            return "impossible";
-        }
-        String str=dfs(startNode,0,k,"",endNode);
-        if(str!=null) return str;
-        return "impossible";
-        
+        return answer;
     }
 }
