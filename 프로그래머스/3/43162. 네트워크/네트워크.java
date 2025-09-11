4e73b9eff70d41804d1a8d25b075a39782d56fee
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-11 14:34:20 +0900

    [level 3] Title: 네트워크, Time: 0.87 ms, Memory: 89.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,56 @@
-class Solution {
-    private static boolean[] visited;
-    private static int[][] computer;
-    
-    public void dfs(int now){
-        visited[now]=true;
-        for(int i=0;i<computer[now].length;i++){
-            if(computer[now][i]==1 && !visited[i]){
-                dfs(i);
+import java.util.*;
+
+ class Solution {
+        private static int[] arr;
+        private Set<Integer> set;
+        private static int getParent(int a){
+            if(a==arr[a]) return a;
+            return arr[a]=getParent(arr[a]);
+        }
+        private static void union(int a,int b){
+            a=getParent(a);
+            b=getParent(b);
+            if(a>b){
+                for(int i=0;i<arr.length;i++){
+                    if(a==arr[i]) arr[i]=b;
+                }
+                arr[a]=b;
+            }else{
+                for(int i=0;i<arr.length;i++){
+                    if(b==arr[i]) arr[i]=a;
+                }
+                arr[b]=a;
             }
         }
-    }
-    public int solution(int n, int[][] computers) {
-        computer=computers;
-        visited=new boolean[n];
-        int answer = 0;
-        
-        for(int i=0;i<n;i++){
-            if(!visited[i]){
-                dfs(i);
-                answer++;
-            }
+        private static boolean isSameParent(int a,int b){
+            a=getParent(a);
+            b=getParent(b);
+            return a==b;
         }
-        return answer;
+
+
+        public int solution(int n, int[][] computers) {
+            arr=new int[n];
+            for(int i=0;i<n;i++){
+                arr[i]=i;
+            }
+
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(i==j) continue;
+                    //네트워크가 연결되어 있는 경우
+                    if(computers[i][j]==1){
+                        //이미 같은 집합인 경우
+                        if(isSameParent(i,j)) continue;
+                        //다른 집합인 경우
+                        union(i,j);
+                    }
+                }
+            }
+            set=new HashSet<>();
+            for(int x:arr){
+                set.add(x);
+            }
+            return set.size();
+        }
     }
-}
