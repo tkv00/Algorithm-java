d26c43b54375c6aca8421bc49cf13959da3cc48e
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-11 10:30:37 +0900

    [level 1] Title: 폰켓몬, Time: 106.98 ms, Memory: 87.4 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,18 +1,37 @@
-import java.util.HashSet;
+import java.util.*;
 
 class Solution {
+    private static int[] arr;
+    private static Set<Integer> set;
+    private static int getParent(int a){
+        if(arr[a]==a) return a;
+        return arr[a]=getParent(arr[a]);
+    }
+    private static void union(int a,int b){
+        a=getParent(a);
+        b=getParent(b);
+        if(a>b) arr[a]=b;
+        else arr[b]=a;
+    }
+    private static int max;
     public int solution(int[] nums) {
-        int result=0;
-        HashSet<Integer> set=new HashSet<>();
-        int size=nums.length;
-        for(int i=0;i<size;i++){
-            set.add(nums[i]);
+        arr=new int[nums.length];
+        
+        for(int i=0;i<nums.length;i++){
+            arr[i]=i;
         }
-        if(size/2>set.size()){
-            result=set.size();
-        }else{
-            result=size/2;
+        
+        for(int i=0;i<nums.length-1;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]==nums[j]) union(arr[i],arr[j]);    
+            }
         }
-        return result;
+        set=new HashSet<>();
+        for(int x:arr){
+            set.add(x);
+        }
+
+        
+        return Math.min(nums.length/2 , set.size());
     }
 }
