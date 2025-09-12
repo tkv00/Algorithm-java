39e3230f5f62c33432bc20476d5d52ab32b892f4
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-12 14:38:53 +0900

    [level 3] Title: 가장 먼 노드, Time: 26.98 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,47 +1,60 @@
 import java.util.*;
 class Solution {
-    private static int answer=0;
-    private static int MAX_VALUE=Integer.MIN_VALUE;
+    private static int cnt;
+    private static boolean[] visited;
+    private static ArrayDeque<Node> dq;
+    private static ArrayList<Integer>[] map;
+    private static int[] distance;
+    private static class Node{
+        int index;
+        int cnt;
+        
+        Node(int index,int cnt){
+            this.index=index;
+            this.cnt=cnt;
+        }
+    }
         
     public int solution(int n, int[][] edge) {
-        Deque<Integer> q=new ArrayDeque<>();
-        ArrayList<Integer>[] array=new ArrayList[n+1];
-        boolean[]visit=new boolean[n+1];
-        int[] dist=new int[n+1];
+        map=new ArrayList[n+1];
+        distance=new int[n+1];
+        dq=new ArrayDeque<>();
+        visited=new boolean[n+1];
         
-        for(int i=0;i<=n;i++){
-            array[i]=new ArrayList<Integer>();
-        }
-        for(int i=0;i<edge.length;i++){
-            int first=edge[i][0];
-            int second=edge[i][1];
-            array[first].add(second);
-            array[second].add(first);
+        for(int i=1;i<=n;i++){
+            map[i]=new ArrayList<>();
         }
         
-        q.addLast(1);
-        visit[1]=true;
-        int dep=0;
+        for(int[] info:edge){
+            map[info[0]].add(info[1]);
+            map[info[1]].add(info[0]);
+        }
         
-        while(!q.isEmpty()){
-            int now=q.pollFirst();
-            dep=dist[now]+1;
-            for(int next:array[now]){
-               if(!visit[next]){
-                    //방문처리
-                   visit[next]=true;
-                   dist[next]=dep;
-                   q.addLast(next);
-                   MAX_VALUE=Math.max(MAX_VALUE,dist[next]);
-               }
+        dq.offer(new Node(1,1));
+        visited[1]=true;
+        distance[1]=1;
+        
+        while(!dq.isEmpty()){
+            Node now=dq.poll();
+            
+            for(int x:map[now.index]){
+                if(!visited[x]){
+                    distance[x]=now.cnt+1;
+                    visited[x]=true;
+                    dq.offer(new Node(x,now.cnt+1));
+                }
             }
         }
         
-        for(int k:dist){
-            if(k==MAX_VALUE){
-                answer++;
-            }
+        int max=Integer.MIN_VALUE;
+        for(int x:distance){
+            max=Math.max(x,max);
         }
-        return answer;
+        
+        int result=0;
+        for(int x:distance){
+            if(x==max) result++;
+        }
+        return result;
     }
 }
