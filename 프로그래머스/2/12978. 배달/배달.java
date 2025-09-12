3d983dfd39d687f66e2f4c3cc88f2263275c6085
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-09-12 10:23:22 +0900

    [level 2] Title: 배달, Time: 8.59 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,39 +1,38 @@
 import java.util.*;
 
 class Solution {
-    private static int[][] map;
-    private static final int INF=Integer.MAX_VALUE;
+    private static int[][]map;
+    private static final int INF=20_000_001;
     public int solution(int N, int[][] road, int K) {
         map=new int[N+1][N+1];
-        int cnt=0;
         for(int i=1;i<=N;i++){
-            Arrays.fill(map[i],INF);
+            for(int j=1;j<=N;j++){
+                if(i==j) continue;
+                map[i][j]=INF;
+            }
         }
-        
-        for(int i=1;i<=N;i++){
-            map[i][i]=0;
-        }
-        for(int[] info:road){
-            int row=info[0];
-            int col=info[1];
-            map[row][col]=Math.min(map[row][col],info[2]);
-            map[col][row]=Math.min(map[col][row],info[2]);
+    
+        for(int[] arr:road){
+            map[arr[0]][arr[1]]=
+                    Math.min(arr[2],map[arr[0]][arr[1]]);
+            map[arr[1]][arr[0]]= 
+                    Math.min(arr[2],map[arr[1]][arr[0]]);
         }
         
         for(int k=1;k<=N;k++){
             for(int i=1;i<=N;i++){
                 for(int j=1;j<=N;j++){
-                    if(i==j || i==k || j==k) continue;
-                    if(map[i][k]!=INF && map[k][j]!=INF){
-                        map[i][j]=Math.min(map[i][j],map[i][k]+map[k][j]);        
-                    }
-                
+                    if(i==j) continue;
+                    map[i][j]=Math.min(map[i][j],map[i][k]+map[k][j]);
                 }
             }
         }
+        
+        int answer=0;
         for(int i=1;i<=N;i++){
-            if(map[1][i]<=K)cnt++;
+            if(map[1][i]<=K) answer++;
         }
-        return cnt;
+        return answer;
+        
     }
 }
