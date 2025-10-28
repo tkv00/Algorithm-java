8d395d6fa2b2a4f8851fa6cc9da4df7f68ee74c3
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-28 18:16:07 +0900

    [Silver III] Title: 1로 만들기, Time: 124 ms, Memory: 18164 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,37 +1,28 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
-    public static int N;
-    public static int []dp;
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        dp=new int[N+1];
-        dp[0]=0;
-        for(int i=1;i<N+1;i++){
-            dp[i]=i-1;
-        }
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        int[] dp = new int[num + 1];
 
-        for(int i=2;i<N;i++){
-            dp[i+1]=dp[i]+1;
-            if((i+1)%3==0){
-
-                dp[i+1]=Math.min(dp[i+1],dp[(i+1)/3]+1);
-               // System.out.println(i+1+" "+dp[i+1]);
-            }
-            if((i+1)%2==0){
-                dp[i+1]=Math.min(dp[i+1],dp[(i+1)/2]+1);
+        if (num > 1) {
+            dp[2] = 1;
+            if (num > 2) {
+                dp[3] = 1;
             }
 
         }
-        //System.out.println(Arrays.toString(dp));
 
-        System.out.println(dp[N]);
-
-
+        for (int i = 4; i <= num; i++) {
+            if (i%6==0) dp[i]=Math.min(dp[i/2]+1,Math.min(dp[i/3]+1,dp[i-1]+1));
+            else if (i % 2 == 0) dp[i] = Math.min(dp[i / 2] + 1, dp[i - 1] + 1);
+            else if (i % 3 == 0) dp[i] = Math.min(dp[i / 3] + 1, dp[i - 1] + 1);
+            else dp[i] = dp[i - 1] + 1;
+        }
+        System.out.println(dp[num]);
     }
-
 }
