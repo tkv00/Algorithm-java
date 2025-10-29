163c0150bb208d55296176d675f041ca5000e6f9
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-29 14:54:47 +0900

    [Gold IV] Title: N-Queen, Time: 8100 ms, Memory: 19600 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -4,37 +4,55 @@ import java.io.InputStreamReader;
 
 public class Main {
     private static int N;
-    private static int[] map;
-    private static int result=0;
-    private static void DFS(int depth){
-        if(depth==N){
+    private static BufferedReader br;
+    private static int result = 0;
+    private static boolean[][] visited;
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        visited = new boolean[N][N];
+    }
+
+    private static void backTracking(int row) {
+        if (row >= N) {
             result++;
             return;
         }
 
-        for(int i=0;i<N;i++){
-            //depth:행  i:열
-            map[depth]=i;
-            if(isValid(depth)){
-                DFS(depth+1);
+
+        for (int col=0;col<N;col++){
+            if (isSafe(row,col)){
+                visited[row][col]=true;
+                backTracking(row+1);
+                visited[row][col]=false;
             }
         }
     }
-    private static boolean isValid(int row){
-        for(int i=0;i<row;i++){
-            //열이 같은 경우
-            if(map[row]==map[i]) return false;
-            
-            //대각선이 같은 경우
-            if(Math.abs(row-i)==Math.abs(map[row]-map[i])) return false;
+
+    private static boolean isSafe(int row, int col) {
+        //열 검사
+        for (int i = 0; i < row; i++) {
+            if (visited[i][col]) return false;
         }
+
+        //왼쪽 위 대각선
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (visited[i][j]) return false;
+        }
+
+        //오른쪽 위 대각선
+        for (int i = row - 1, j = col + 1; i >= 0 && j < N; i--, j++) {
+            if (visited[i][j]) return false;
+        }
+
         return true;
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        map=new int[N];
-        DFS(0);
+
+    public static void main(String[] args) throws IOException {
+        init();
+        backTracking(0);
+
         System.out.println(result);
-    }    
+    }
 }
