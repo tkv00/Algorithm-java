1a352a74126f9f93a4b54b8ed7d0794fb78aaf3b
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-29 16:50:46 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 268 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -1,103 +1,117 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, K;
-    static int[] arr;
-    static boolean[] robot;
-    static StringTokenizer st;
-    static int zero = 0;
-    static int cnt = 0;
+    private static int N, K;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int[] belt;
+    private static boolean[] robots; //로봇 위치 메서드
+    private static int step = 0;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        arr = new int[2 * N];
-        robot = new boolean[N];
+        belt = new int[2 * N];
+        robots = new boolean[N];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 2 * N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            belt[i] = Integer.parseInt(st.nextToken());
         }
-        while (true) {
-            //==올리는 위치에서 로봇 올리기==
-            cnt += 1;
 
-            //1단계 -> 벨트 회전
-            rotateRobot(robot);
-            rotate(arr);
-
-            //로봇이 제일 끝이라면
-            if(robot[N-1]){
-                //내리기
-                robot[N-1]=false;
-            }
-
-
-            //2단계 -> 가장 먼저 올라간 로봇 부터 벨트 회전 방향으로 이동
-            for (int i = N - 1; i > 0; i--) {
-                //i에 로봇이 없어야 함.
-
-                if (robot[i - 1] && !robot[i] && arr[i] > 0) {
-                    //로봇 이동
-                    robot[i - 1] = false;
-                    robot[i] = true;
-                    //내구도 감소
-                    arr[i] -= 1;
-                }
-
-            }
-            //로봇이 제일 끝이라면
-            if(robot[N-1]){
-                robot[N-1]=false;
-            }
-
-            //3단계 -> 올리는 위치에 내구도 0이아니면
-            if (arr[0] > 0) {
-                robot[0] = true;
-                //내구도 감소
-                arr[0] -= 1;
-            }
-            //로봇이 제일 끝이라면
-            if (zero(arr) >= K) {
-                //내리기
-                break;
-            }
-
-        }
-        System.out.println(cnt);
     }
 
-    //한 칸씩 이동 하는 로직
-    static void rotate(int[] arr) {
-        //마지막 수 저장
-        int end = arr[arr.length - 1];
-        for (int i = arr.length - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
+    //벨트 회전 함수
+    private static void rotationBelt() {
+        int temp = belt[2 * N - 1];
+
+        for (int i = 2 * N - 1; i > 0; i--) {
+            belt[i]=belt[i-1];
         }
-        arr[0] = end;
+
+        belt[0]=temp;
     }
 
-    static void rotateRobot(boolean[] robot) {
-        for (int i = N - 1; i > 0; i--) {
-            robot[i] = robot[i - 1];
+    //로봇 회전 함수
+    private static void rotationRobot() {
+        for (int i = N-1; i > 0 ; i--) {
+           robots[i]=robots[i-1];
         }
-        robot[0] = false;
+
+        robots[0]=false;
+        robots[N-1]=false;
     }
 
-    static int zero(int[] arr) {
+    //내구도가 0인 칸 K이상 검증 메서드
+    private static boolean isEnd() {
         int cnt = 0;
-        for (int j : arr) {
-            if (j == 0) {
-                cnt += 1;
-            }
+        for (int i = 0; i < 2 * N; i++) {
+            if (belt[i] == 0) cnt++;
         }
-        return cnt;
+        return cnt >= K;
+    }
+
+    private static void operation() {
+        while (!isEnd()) {
+
+            /**
+             * 올리는 위치 index : 0
+             * 내리는 위치 index : N-1
+             */
+            //1단계 - 로봇+벨트 회전
+            rotationRobot();
+            rotationBelt();
+
+            //2단계 - 0~N-1까지 로봇
+            for (int i =N-2;i>=0;i--) {
+                if (robots[i]) {
+                    //이동하려는 칸에 로봇 X + 내구도 1이상
+                    if (!robots[i + 1] && belt[i + 1] >= 1) {
+                        //로봇 이동
+                        robots[i + 1] = true;
+                        robots[i] = false;
+
+                        //내구도 1감소
+                        belt[i + 1]--;
+                    }
+                }
+            }
+            robots[N-1]=false;
+
+            //3단계 - 올리는 위치에 로봇 올리기
+            if (belt[0] > 0) {
+                belt[0]--;
+                robots[0] = true;
+            }
+
+            step++;
+        }
+
+    }
+
+    private static void printMap() {
+        for (int i = 0; i < 2 * N; i++) {
+            System.out.print(belt[i] + " ");
+        }
+        System.out.println();
+        System.out.println("=================");
+        for (int i = 0; i < N; i++) {
+            System.out.print(robots[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        operation();
+
+        System.out.println(step);
     }
 }
