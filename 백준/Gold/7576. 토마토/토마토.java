34512298a68498e56c2946de5f0d6e78ae966825
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-29 15:21:17 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 124904 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,104 +1,89 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N;
-    public static int M;
+    private static int N,M;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static int[][]map;
+    private static int[][]distMap;
+    private static Queue<int[]> tomatoes;
+    private static int[] dr=new int[]{0,0,1,-1};
+    private static int[] dc=new int[]{1,-1,0,0};
+    private static int day=0;
 
-    public static int[][] array;
-    public static boolean[][] visited;
-    public static StringTokenizer st;
-    public static Queue<int[]> queue=new LinkedList<>();
-
-    public static int day = 0;
-    public static int[] dx = {1, 0, -1, 0};
-    public static int[] dy = {0, -1, 0, 1};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-
-        array = new int[M][N];
-        visited = new boolean[M][N];
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < N; j++) {
-                array[i][j] = Integer.parseInt(st.nextToken());
-
-            }
-
-        }
-
-        for (int i = 0; i < M; i++) {
-
-            for (int j = 0; j < N; j++) {
-                if(array[i][j]==1){
-                    visited[i][j]=true;
-                    queue.add(new int[]{i,j});
-
-                }
-            }
-
-        }
-        if(isAll()){
-            System.out.println(0);
-            return;
-        }
-        bfs();
-        if(!isAll()){
-            System.out.println(-1);
-        }  else{
-            System.out.println(day-1);
-        }
-        //에제3,5 틀림.
-
-
-
-
-
+    private static boolean isValid(int row,int col){
+        return row>=0 && row<N && col>=0 && col<M && map[row][col]!=-1;
     }
+    private static void init() throws IOException {
+        br=new BufferedReader(new InputStreamReader(System.in));
+        st=new StringTokenizer(br.readLine());
+        tomatoes=new LinkedList<>();
 
-    public static void bfs() {
+        M=Integer.parseInt(st.nextToken());
+        N=Integer.parseInt(st.nextToken());
 
+        map=new int[N][M];
+        distMap=new int[N][M];
 
-        while(!queue.isEmpty()){
-            int []now=queue.poll();
-            int preX=now[0];
-            int preY=now[1];
-            for(int i=0;i<4;i++){
-                int new_x=preX+dx[i];
-                int new_y=preY+dy[i];
-                if((new_x>=0 && new_x<M) && (new_y>=0 && new_y<N)&&
-                        (array[new_x][new_y]==0)&&(!visited[new_x][new_y])){
-                    array[new_x][new_y]=array[preX][preY]+1;
-                    day=Math.max(day,array[new_x][new_y]);
-
-                    visited[new_x][new_y]=true;
-                    queue.add(new int[]{new_x,new_y});
-
-
+        for (int i=0;i<N;i++){
+            st=new StringTokenizer(br.readLine());
+            for (int j=0;j<M;j++){
+                map[i][j]=Integer.parseInt(st.nextToken());
+                if (map[i][j]==1) {
+                    tomatoes.offer(new int[]{i, j});
+                    distMap[i][j]=1;
                 }
             }
         }
-
     }
 
-    //모두 다 익었는가
-    public static boolean isAll(){
-        for(int i=0;i<M;i++){
-            for(int j=0;j<N;j++){
-                if(array[i][j]==0)
-                    return false;
+    private static void BFS(){
+        if (tomatoes.isEmpty()) return;
+
+        while (!tomatoes.isEmpty()){
+            int[]now=tomatoes.poll();
+
+            for (int d=0;d<4;d++){
+                int nr=now[0]+dr[d];
+                int nc=now[1]+dc[d];
+
+                if (!isValid(nr,nc)) continue;
+                if (distMap[nr][nc]!=0) continue;
+
+                distMap[nr][nc]=distMap[now[0]][now[1]]+1;
+                tomatoes.offer(new int[]{nr,nc});
+                map[nr][nc]=1;
+                day=Math.max(day,distMap[nr][nc]-1);
+            }
+        }
+    }
+
+    private static void printMap(){
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                System.out.print(distMap[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean isEnd(){
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                if (map[i][j]==0) return false;
             }
         }
         return true;
     }
-
+    public static void main(String[] args) throws IOException {
+        init();
+        BFS();
+        //printMap();
+        System.out.println(isEnd() ? day : -1);
+    }
 }
