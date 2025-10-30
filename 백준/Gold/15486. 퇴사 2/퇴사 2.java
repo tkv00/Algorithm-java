e402f7bd149fc6638c768c74f903bbf04e9b6ee6
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-30 16:24:49 +0900

    [Gold V] Title: 퇴사 2, Time: 760 ms, Memory: 338892 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,42 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int n;
-    public static int[][]arr;
-    public static StringTokenizer st;
-    public static int []dp;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        n=Integer.parseInt(br.readLine());
+    private static class Counsel{
+        int start;
+        int end;
+        int cost;
 
-        arr=new int[2][n+2];
-        for(int i=1;i<=n;i++){
+        Counsel(int start,int end,int cost){
+            this.start=start;
+            this.end=end;
+            this.cost=cost;
+        }
+    }
+    private static int N;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static Counsel[] inputs;
+    private static int[] dp;
+    private static int max=0;
+
+    private static void init() throws IOException {
+        br=new BufferedReader(new InputStreamReader(System.in));
+        N=Integer.parseInt(br.readLine());
+        inputs=new Counsel[N+1];
+        dp=new int[N+1];
+
+        for (int day=1;day<=N;day++){
             st=new StringTokenizer(br.readLine());
-            int day=Integer.parseInt(st.nextToken());
-            int cost=Integer.parseInt(st.nextToken());
-            arr[0][i]=day;
-            arr[1][i]=cost;
-        }
-        dp=new int[n+2];
-        int max=-1;
 
-        for(int i=1;i<=n+1;i++){
-            if(dp[i]>max){
-                max=dp[i];
-            }
-            int next=i+arr[0][i];
-            if(next<n+2){
-                dp[next]=Math.max(dp[next],max+arr[1][i] );
-            }
+            int T=Integer.parseInt(st.nextToken());//걸리는 날짜
+            int P=Integer.parseInt(st.nextToken());//받을 수 있는 금액
+
+            inputs[day]=new Counsel(day,day+T-1,P);
 
         }
+    }
+
+    private static void dynamicProgramming(){
+        for (int i=1;i<=N;i++){
+            Counsel now=inputs[i];
+            //상담을 하지 않는 case
+            dp[i]=Math.max(dp[i-1],dp[i]);
+
+            //상담을 하는 경우
+            if (now.end>N) continue;
+            dp[now.end]=Math.max(dp[now.end],now.cost+dp[now.start-1]);
+
+            //System.out.println("day : "+i+" "+Arrays.toString(dp));
+
+            max=Math.max(max,dp[now.end]);
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        init();
+        dynamicProgramming();
+        //System.out.println(Arrays.toString(dp));
         System.out.println(max);
     }
 }
-
-
-
