8ec41a50defb237d4f4b66c2facd7bcc54c05e17
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-10-31 11:44:37 +0900

    [Gold V] Title: 노드사이의 거리, Time: 188 ms, Memory: 21540 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.java
@@ -2,82 +2,72 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static class Node {
-        int link;
+    private static int N,M;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static List<int[]>[] graph;
+    private static boolean[] visited;
+    private static StringBuilder sb;
+    private static List<int[]> query;
+    private static void init() throws IOException {
+        br=new BufferedReader(new InputStreamReader(System.in));
+        st=new StringTokenizer(br.readLine());
 
-        int cost;
+        N=Integer.parseInt(st.nextToken());
+        M=Integer.parseInt(st.nextToken());
 
-        Node(int link, int cost) {
-            this.cost = cost;
-            this.link = link;
+        graph=new List[N+1];
+        query=new ArrayList<>();
+        sb=new StringBuilder();
 
+        for (int i=1;i<=N;i++){
+            graph[i]=new ArrayList<>();
+        }
+
+        for (int i=0;i<N-1;i++){
+            st=new StringTokenizer(br.readLine());
+            int start=Integer.parseInt(st.nextToken());
+            int end=Integer.parseInt(st.nextToken());
+            int cost=Integer.parseInt(st.nextToken());
+
+            graph[start].add(new int[]{end,cost});
+            graph[end].add(new int[]{start,cost});
+        }
+
+        for (int i=0;i<M;i++){
+            st=new StringTokenizer(br.readLine());
+            int node1=Integer.parseInt(st.nextToken());
+            int node2=Integer.parseInt(st.nextToken());
+
+            query.add(new int[]{node1,node2});
         }
     }
 
-    public static ArrayList<Node>[] arrayLists;
-    public static int n;
-    public static int m;
-    public static StringTokenizer st;
+    private static void operation(int now,int cost,int find){
+        if (now==find){
+            sb.append(cost).append("\n");
+            return;
+        }
 
+        visited[now]=true;
+
+        for (int[] next:graph[now]){
+            if(visited[next[0]]) continue;
+
+            operation(next[0],cost+next[1],find);
+        }
+    }
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        arrayLists = new ArrayList[n + 1];
-        for (int i = 0; i <= n; i++) {
-            arrayLists[i] = new ArrayList<>();
-        }
-        for (int i = 0; i < n - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            int cost = Integer.parseInt(st.nextToken());
-
-            arrayLists[start].add(new Node(end, cost));
-            arrayLists[end].add(new Node(start, cost));
-        }
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            bfs(start, end);
+        init();
+        for (int[] nodes:query){
+            visited=new boolean[N+1];
+            operation(nodes[0],0,nodes[1]);
         }
 
-    }
-
-    public static void bfs(int start, int end) {
-        Queue<Node> queue = new LinkedList<>();
-        boolean[] visited = new boolean[n + 1];
-        visited[start] = true;
-        queue.offer(new Node(start,0));
-        int totalCost = 0;
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            int  v=node.link;
-            if (v == end) {
-                System.out.println(node.cost);
-                break;
-            }
-            for (Node n : arrayLists[v]) {
-                int s = n.link;
-
-                //방문안 했을때
-                if (!visited[s]) {
-                    visited[s] = true;
-                    queue.add(new Node(s,n.cost+node.cost));
-                }
-
-            }
-
-
-        }
-
+        System.out.println(sb);
     }
 }
