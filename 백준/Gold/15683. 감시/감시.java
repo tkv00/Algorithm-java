3be724b43fff4dbb9c11ed005811f38bb96e1323
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-02 18:56:34 +0900

    [Gold III] Title: 감시, Time: 276 ms, Memory: 58456 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -6,106 +6,189 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class CCTV {
+    private static int N, M;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static int[][] office;
+    private static List<CCTV> cctvs;
+    private static int min = Integer.MAX_VALUE;
+    //우,아래,좌,위
+    private static int[] dr = new int[]{0, 1, 0, -1};
+    private static int[] dc = new int[]{1, 0, -1, 0};
+
+    private static class CCTV {
         int row;
         int col;
-        int type;
-        CCTV(int row, int col, int type) {
-            this.row = row;
+        int num;
+
+        CCTV(int row, int col, int num) {
             this.col = col;
-            this.type = type;
+            this.row = row;
+            this.num = num;
         }
     }
-    static int row;
-    static int col;
-    static int[][] map;
-    static StringTokenizer st;
-    static int res = Integer.MAX_VALUE;
-    static int[] dx = {0, 1, 0, -1};//우하좌상
-    static int[] dy = {1, 0, -1, 0};//우하좌상
-    static List<CCTV> cctvList;
-    static int[][] directions = {
-            {},//0번
-            {0},//1번 cctv
-            {0, 2},//2 cctv
-            {0, 3},//3 cctv
-            {0, 2, 3},//4 cctv
-            {0, 1, 2, 3}// 5 cctv
-    };
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        row = Integer.parseInt(st.nextToken());
-        col = Integer.parseInt(st.nextToken());
-        cctvList = new ArrayList<>();
-        map = new int[row][col];
-        for (int i = 0; i < row; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < col; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-                //cctv라면
-                if (map[i][j] > 0 && map[i][j] <= 5) {
-                    cctvList.add(new CCTV(i, j, map[i][j]));
-                }
-            }
-        }
-        dfs(0,map);
-        System.out.println(res);
-    }
-    static void dfs(int count, int[][] map) {
-        if (count == cctvList.size()) {
-            res = Math.min(res, getResult(map));
+
+    private static void combination(int depth, int[][] map) {
+        //중단 조건
+        if (depth >= cctvs.size()) {
+            min = Math.min(min, count(map));
             return;
         }
-        //cctv위치
-        int cctvRow = cctvList.get(count).row;
-        int cctvCol = cctvList.get(count).col;
-        int type = cctvList.get(count).type;
-        //4방향 0 90 180 270
-        for (int i = 0; i < 4; i++) {
-            int[][] copy = copyArr(map);
-            for (int move : directions[type]) {
-                int newRow = cctvRow;
-                int newCol = cctvCol;
-                int newDir = (move + i) % 4;
-                while (true) {
-                    newRow += dx[newDir];
-                    newCol += dy[newDir];
-                    if (!isValid(newRow,newCol))break;
-                    copy[newRow][newCol]=-1;
-                }
-            }
-            dfs(count+1,copy);
+
+        CCTV now = cctvs.get(depth);
+
+        for (int dir = 1; dir <= 4; dir++) {
+            int[][] newMap = operation(map, now, dir);
+            combination(depth+1,newMap);
         }
     }
-    //사각 지대 구하는 함수
-    static int getResult(int[][] arr) {
+
+    private static int[][] operation(int[][] map, CCTV now, int direction) {
+        int[][] newMap = copy(map);
+
+        int[] selectedDirection = choice(now.num, direction);
+
+        //CCTV가 보는 경로 표시
+        for (int s : selectedDirection) {
+            int row = now.row;
+            int col = now.col;
+
+            while (true) {
+                row += dr[s];
+                col += dc[s];
+
+                if (row < 0 || row >= N || col < 0 || col >= M) break;
+                if (newMap[row][col] == 6) break; //벽인 경우
+
+                //CCTV로 채우기
+                newMap[row][col] = now.num;
+            }
+        }
+
+        return newMap;
+    }
+
+
+    private static int[] choice(int cctv, int dir) {
+        switch (cctv) {
+            case 1: {
+                switch (dir) {
+                    case 1:
+                        return new int[]{0};
+                    case 2:
+                        return new int[]{1};
+                    case 3:
+                        return new int[]{2};
+                    case 4:
+                        return new int[]{3};
+                }
+            }
+            break;
+
+            case 2: {
+                switch (dir) {
+                    case 1:
+                    case 3:
+                        return new int[]{0, 2};
+                    case 2:
+                    case 4:
+                        return new int[]{1, 3};
+                }
+
+            }
+            break;
+
+            case 3: {
+                switch (dir) {
+                    case 1:
+                        return new int[]{0, 3};
+                    case 2:
+                        return new int[]{0, 1};
+                    case 3:
+                        return new int[]{1, 2};
+                    case 4:
+                        return new int[]{2, 3};
+                }
+                break;
+            }
+
+            case 4: {
+                switch (dir) {
+                    case 1:
+                        return new int[]{2, 3, 0};
+                    case 2:
+                        return new int[]{0, 1, 3};
+                    case 3:
+                        return new int[]{0, 1, 2};
+                    case 4:
+                        return new int[]{1, 2, 3};
+                }
+                break;
+            }
+
+            case 5:
+                switch (dir) {
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        return new int[]{0, 1, 2, 3};
+                }
+                break;
+        }
+        return new int[]{};
+    }
+
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        office = new int[N][M];
+        cctvs=new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                office[i][j] = Integer.parseInt(st.nextToken());
+                if (office[i][j] != 0 && office[i][j] < 6) cctvs.add(new CCTV(i, j, office[i][j]));
+            }
+        }
+    }
+
+
+    private static int[][] copy(int[][] origin) {
+        int[][] copyMap = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                copyMap[i][j] = origin[i][j];
+            }
+        }
+        return copyMap;
+    }
+
+    //사각지대 구하기
+    private static int count(int[][] map) {
         int sum = 0;
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                if (arr[i][j] == 0) {
-                    sum += 1;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    sum++;
                 }
             }
         }
         return sum;
     }
-    //배열 깊은 복사
-    static int[][] copyArr(int[][] arr) {
-        int[][] newArr = new int[row][col];
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                newArr[i][j] = arr[i][j];
-            }
-        }
-        return newArr;
-    }
-    //경계검사 함수
-    static boolean isValid(int x, int y) {
-        if (x < 0 || x >= row) return false;
-        if (y < 0 || y >= col) return false;
-        //벽
-        if (map[x][y] == 6) return false;
-        return true;
+
+    public static void main(String[] args) throws IOException {
+        init();
+        combination(0,office);
+
+        System.out.println(min);
     }
 }
