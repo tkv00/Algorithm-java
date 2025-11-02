effa9e620037ce6bcaaccea475903bf2c4cf296d
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-02 20:31:05 +0900

    [Gold IV] Title: 테트로미노, Time: 456 ms, Memory: 30960 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.java
+++ 백준/Gold/14500. 테트로미노/테트로미노.java
@@ -4,60 +4,91 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int M;
-    static int [][] map;
-    static StringTokenizer st;
-    static boolean[][]visited;
-    static int[]dx={1,-1,0,0};
-    static int[]dy={0,0,1,-1};
-    static int res=0;
+    private static int N, M;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static int[][] map;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        M=Integer.parseInt(st.nextToken());
-        map=new int[N][M];
-        visited=new boolean[N][M];
 
-        for(int i=0;i<N;i++){
+    private static int max=0;
+    private static final int[][][] combination = {
+            // ㅡ, ㅣ
+            {{0,0},{0,1},{0,2},{0,3}},
+            {{0,0},{1,0},{2,0},{3,0}},
+
+            // 정사각형
+            {{0,0},{0,1},{1,0},{1,1}},
+
+            // L, J형 (8가지)
+            {{0,0},{1,0},{2,0},{2,1}},
+            {{0,0},{1,0},{2,0},{2,-1}},
+            {{0,0},{0,1},{1,0},{2,0}},
+            {{0,0},{0,1},{1,1},{2,1}},
+            {{0,0},{0,1},{0,2},{1,0}},
+            {{0,0},{0,1},{0,2},{1,2}},
+            {{0,0},{1,0},{1,1},{1,2}},
+            {{0,0},{1,0},{1,-1},{1,-2}},
+
+            // Z, S형 (4가지)
+            {{0,0},{0,1},{1,1},{1,2}},
+            {{0,0},{1,0},{1,-1},{2,-1}},
+            {{0,0},{1,0},{1,1},{2,1}},
+            {{0,0},{0,1},{-1,1},{-1,2}},
+
+            // ㅗ형 (4가지)
+            {{0,0},{0,1},{0,2},{1,1}}, // ㅜ
+            {{0,0},{1,0},{2,0},{1,1}}, // ㅏ
+            {{0,0},{0,1},{0,2},{-1,1}}, // ㅗ
+            {{0,0},{1,0},{2,0},{1,-1}} // ㅓ
+    };
+
+
+    private static void tetromino() {
+        for (int i=0;i<N;i++){
+            for (int j=0;j<M;j++){
+                for (int[][] comb:combination){
+                    int sum=0;
+                    boolean flag=true;
+
+                    for (int d=0;d<4;d++){
+                        int next_row=i+comb[d][0];
+                        int next_col=j+comb[d][1];
+
+                        if (next_row<0 || next_row>=N || next_col<0 ||next_col>=M) {
+                            flag=false;
+                            break;
+                        };
+                        sum+=map[next_row][next_col];
+                    }
+                    if (flag){
+                        max=Math.max(max,sum);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        map = new int[N][M];
+
+        for (int i=0;i<N;i++){
             st=new StringTokenizer(br.readLine());
-            for(int j=0;j<M;j++){
+            for (int j=0;j<M;j++){
                 map[i][j]=Integer.parseInt(st.nextToken());
             }
         }
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                visited[i][j]=true;
-                dfs(j,i,1,map[i][j]);
-                visited[i][j]=false;
-            }
-        }
-        System.out.println(res);
-
-
     }
-     static void dfs(int x,int y,int depth,int hap){
-        if(depth==4){
-            res=Math.max(res,hap);
-            return;
-        }
-        for(int i=0;i<4;i++){
-            int newx=x+dx[i];
-            int newy=y+dy[i];
-            if((newx>=0 && newx<M)&&(newy>=0 && newy<N)&&!visited[newy][newx]){
-                //ㅗ모양 탐색
-                if(depth==2){
-                    visited[newy][newx]=true;
-                    dfs(x,y,depth+1,hap+map[newy][newx]);
-                    visited[newy][newx]=false;
-                }
 
-                visited[newy][newx]=true;
-                dfs(newx,newy,depth+1,hap+map[newy][newx]);
-                visited[newy][newx]=false;
-            }
-        }
+    public static void main(String[] args) throws IOException {
+        init();
+        tetromino();
+
+        System.out.println(max);
     }
 }
