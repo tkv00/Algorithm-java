1f7046de717dcd3e3cf596f3630b76cd3fbd548e
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-03 14:57:09 +0900

    [Gold IV] Title: 거짓말, Time: 104 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -2,104 +2,97 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int peopleNum;
-    public static int partyNum;
-    public static int knowNum;
-    public static int[] know;
-    public static int[] arr;
-    public static boolean[] list;
-    public static StringTokenizer st;
-    public static ArrayList<Integer>[] party;
+    private static int N, M;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static List<Integer>[] parties;
+    private static int result = 0;
 
-    public static int getParent(int a,int []arr){
-        if(a==arr[a]) return a;
-        else return arr[a]=getParent(arr[a],arr);
+    private static int findParent(int a) {
+        if (parents[a] == a) return a;
+
+        return parents[a] = findParent(parents[a]);
     }
-    public static void Union(int a,int b,int []arr){
-        a=getParent(a,arr);
-        b=getParent(b,arr);
-        if(a>b) arr[a]=b;
-        else arr[b]=a;
+
+    private static void union(int a, int b) {
+        a = findParent(a);
+        b = findParent(b);
+
+        if (a > b) parents[a] = b;
+        else parents[b] = a;
     }
-    public static int res(){
-        int k=0;
-        for(int i=0;i<=partyNum;i++){
-            if(list[i]) k++;
+
+    private static int[] parents;
+    private static boolean[] knowPeople;
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());//사람 수
+        M = Integer.parseInt(st.nextToken());//파티 수
+
+        parents = new int[N + 1];
+        parties = new List[M];
+        knowPeople=new boolean[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            parents[i] = i;
         }
 
-        return k;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        st=new StringTokenizer(br.readLine());
-
-        peopleNum=Integer.parseInt(st.nextToken());
-        partyNum=Integer.parseInt(st.nextToken());
-
-        party= new ArrayList[partyNum+1];
-        for(int i=0;i<=partyNum;i++){
-            party[i]=new ArrayList<>();
+        st = new StringTokenizer(br.readLine());
+        int people = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < people; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            knowPeople[num] = true;
         }
 
-        st=new StringTokenizer(br.readLine());
-        knowNum=Integer.parseInt(st.nextToken());
+        // 각 파티의 참석자 입력
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int count = Integer.parseInt(st.nextToken());
+            parties[i] = new ArrayList<>();
 
-        arr=new int[peopleNum+1];
-        for(int i=1;i<=peopleNum;i++){
-            arr[i]=i;
-        }
-
-        know=new int[knowNum];
-        //아는 사람 번호 입력받기
-        for(int i=0;i<knowNum;i++){
-            know[i]=Integer.parseInt(st.nextToken());
-        }
-        //각 파티 참가자들 리스트에 넣기
-        for(int i=1;i<=partyNum;i++){
-            st=new StringTokenizer(br.readLine());
-            int size=Integer.parseInt(st.nextToken());
-            for(int j=0;j<size;j++){
-                party[i].add(Integer.parseInt(st.nextToken()));
+            for (int j = 0; j < count; j++) {
+                parties[i].add(Integer.parseInt(st.nextToken()));
             }
         }
-        //System.out.println(Arrays.toString(know));
-       // System.out.println(Arrays.toString(party));
+    }
 
-        for(int i=1;i<=partyNum;i++){
-            int firstNum=party[i].get(0);
-            for(int j=0;j<party[i].size();j++){
-                Union(firstNum,party[i].get(j),arr);
+    private static void unionPeople() {
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < parties[i].size() - 1; j++) {
+                union(parties[i].get(j), parties[i].get(j + 1));
             }
         }
-        list=new boolean[partyNum+1];
-        Arrays.fill(list,true);
+    }
 
-       // System.out.println(Arrays.toString(arr));
-        int cnt=0;
-        for(int i=0;i<knowNum;i++){
-            int knowIdx=getParent(know[i],arr);//알고 있는 사람의 그룹 번호
-            for(int j=1;j<=partyNum;j++){
-                for(int person:party[j]){
-                    if(getParent(person,arr)==knowIdx){
-                        list[j]=false;
+    private static void operation() {
+        for (int i = 0; i < M; i++) {
+            boolean flag=true;
+            for (int person:parties[i]){
+                int root=findParent(person);
+                for (int k=1;k<=N;k++){
+                    if (knowPeople[k] && root==findParent(k)){
+                        flag=false;
                         break;
                     }
                 }
             }
-
+            if (!flag) continue;
+            result++;
         }
-        for(int i=1;i<=partyNum;i++){
-            if(list[i]) cnt++;
-        }
-        System.out.println(cnt);
+    }
 
+    public static void main(String[] args) throws IOException {
+        init();
+        unionPeople();
+        operation();
 
-
-
-
+        System.out.println(result);
     }
 }
