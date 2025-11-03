64efea547fec19fd6f4b9b0a2e955d526a22cfde
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-03 11:26:28 +0900

    [Gold III] Title: 홍익 투어리스트, Time: 700 ms, Memory: 84180 KB -BaekjoonHub

--- 백준/Gold/23326. 홍익 투어리스트/홍익 투어리스트.java
+++ 백준/Gold/23326. 홍익 투어리스트/홍익 투어리스트.java
@@ -1,79 +1,93 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.util.TreeSet;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    private static int N,Q;
-    private static StringBuilder sb=new StringBuilder();
+    private static int N, Q;
     private static StringTokenizer st;
     private static BufferedReader br;
-    private static TreeSet<Integer> tree;
-    /*
-     *
-     * @param num : 도현이의 현재 위치 인덱스 값.
-     * @return 시계 방향으로 이동해야 하는 최소 거리.
-     */
-    private static int getDistance(int num,int size){
-        if(tree.isEmpty()) return -1;
-        if(tree.contains(num)) return 0;
-        int max=tree.last();
+    private static final int ORDER = 1;
+    private static final int MOVE = 2;
+    private static final int PRINT = 3;
+    //도현이 현재 위치
+    private static int pos = 1;
+    private static TreeSet<Integer> set;
+    private static StringBuilder sb;
 
-        Integer ceiling=tree.ceiling(num+1);
-        if(ceiling==null){
-            return size+tree.first()-num;
-        }else{
-            return ceiling-num;
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        set = new TreeSet<>();
+        sb = new StringBuilder();
+
+        N = Integer.parseInt(st.nextToken());
+        Q = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int loc = 1; loc <= N; loc++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num==1) set.add(loc);
         }
-        
+
+        for (int i = 0; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int order = Integer.parseInt(st.nextToken());
+
+            if (order == PRINT) {
+                print();
+            } else {
+                int num = Integer.parseInt(st.nextToken());
+
+                if (order == ORDER) {
+                    specify(num);
+                } else if (order == MOVE) {
+                    move(num);
+                }
+            }
+        }
     }
+
+    //1번
+    private static void specify(int loc) {
+        //명소 지정 해제
+        if (set.contains(loc)) {
+            set.remove(loc);
+        }
+        //명소 지정
+        else {
+            set.add(loc);
+        }
+    }
+
+    //2번
+    private static void move(int x) {
+        pos = (pos + x - 1) % N + 1;
+    }
+
+    //3번
+    private static void print() {
+
+        if (set.isEmpty()){
+            sb.append(-1).append("\n");
+            return;
+        }
+
+        Integer next=set.ceiling(pos);
+
+        //시계 방향으로 돌지 않아도 되는 경우
+        if (next!=null){
+            sb.append(next-pos).append("\n");
+        }
+        //시계 방향으로 돌아야 하는 경우
+        else{
+            int first=set.first();
+            sb.append(N-pos+first).append("\n");
+        }
+    }
+
+
     public static void main(String[] args) throws IOException {
-
-        br=new BufferedReader(new InputStreamReader(System.in));
-        tree=new TreeSet<>((a,b)->a-b);
-        st=new StringTokenizer(br.readLine());
-        N=Integer.parseInt(st.nextToken());
-        Q=Integer.parseInt(st.nextToken());
-        st=new StringTokenizer(br.readLine());
-
-        for(int i=0;i<N;i++){
-            int x=Integer.parseInt(st.nextToken());
-            //명소 목록 삽입.
-            if(x==1)
-                tree.add(i);
-        }
-        //도현이의 현재 위치
-        int now=0;
-
-        for(int i=0;i<Q;i++){
-            int q=0,x=0;
-            st=new StringTokenizer(br.readLine());
-            q=Integer.parseInt(st.nextToken());
-            if(st.hasMoreTokens()){
-                x=Integer.parseInt(st.nextToken());
-            }
-
-            switch(q){
-                case 1:
-                    //명소인 경우
-                    if(tree.contains(x-1)){
-                        tree.remove(x-1);
-                    }
-                    //명소가 아닌 경우
-                    else{
-                        tree.add(x-1);
-                    }
-                    break;
-                case 2:
-                    now=(now+x)%N;
-                    break;
-                case 3:
-                    sb.append(getDistance(now,N)).append("\n");
-                    break;
-            }
-        }
-
-        System.out.print(sb.toString());
+        init();
+        System.out.print(sb);
     }
 }
