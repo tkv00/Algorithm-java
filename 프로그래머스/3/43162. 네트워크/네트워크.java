b18970629c943f8c41e8ed21798700e6602a8209
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-11 14:42:39 +0900

    [level 3] Title: 네트워크, Time: 0.35 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,56 +1,42 @@
 import java.util.*;
 
  class Solution {
-        private static int[] arr;
-        private Set<Integer> set;
-        private static int getParent(int a){
-            if(a==arr[a]) return a;
-            return arr[a]=getParent(arr[a]);
+    private static int[] arr;
+    private static Set<Integer> set;
+     private static void union(int a,int b){
+         a=findParent(a);
+         b=findParent(b);
+         
+         if(a<b) arr[b]=a;
+         else arr[a]=b;
+     }
+     
+     private static int findParent(int a){
+         if(a==arr[a]) return a;
+         else return a=findParent(arr[a]);
+     }
+     
+    public int solution(int n, int[][] computers) {
+        int size=computers.length;
+        arr=new int[size];
+        set=new HashSet<>();
+        
+        for(int i=0;i<size;i++){
+            arr[i]=i;
         }
-        private static void union(int a,int b){
-            a=getParent(a);
-            b=getParent(b);
-            if(a>b){
-                for(int i=0;i<arr.length;i++){
-                    if(a==arr[i]) arr[i]=b;
-                }
-                arr[a]=b;
-            }else{
-                for(int i=0;i<arr.length;i++){
-                    if(b==arr[i]) arr[i]=a;
-                }
-                arr[b]=a;
-            }
-        }
-        private static boolean isSameParent(int a,int b){
-            a=getParent(a);
-            b=getParent(b);
-            return a==b;
-        }
-
-
-        public int solution(int n, int[][] computers) {
-            arr=new int[n];
-            for(int i=0;i<n;i++){
-                arr[i]=i;
-            }
-
-            for(int i=0;i<n;i++){
-                for(int j=0;j<n;j++){
-                    if(i==j) continue;
-                    //네트워크가 연결되어 있는 경우
-                    if(computers[i][j]==1){
-                        //이미 같은 집합인 경우
-                        if(isSameParent(i,j)) continue;
-                        //다른 집합인 경우
-                        union(i,j);
-                    }
+        
+        for(int row=0;row<size;row++){
+            for(int col=row+1;col<size;col++){
+                if(computers[row][col]==1){
+                    union(row,col);
                 }
             }
-            set=new HashSet<>();
-            for(int x:arr){
-                set.add(x);
-            }
-            return set.size();
         }
+        int cnt=1;
+        for(int i=0;i<size;i++){
+            set.add(findParent(i));
+        }
+        
+        return set.size();
     }
+}
