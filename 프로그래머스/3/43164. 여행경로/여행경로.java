12c9b463cbdb4773722c83224de040dc0f7a3e44
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-11 15:35:01 +0900

    [level 3] Title: 여행경로, Time: 1.01 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,35 +1,40 @@
 import java.util.*;
 class Solution {
-    private static int ticketsCnt;
-    private static ArrayList<String> answer=new ArrayList<>();
-    private static boolean[] visit;
+    private static List<String> result;
+    private static Map<String,PriorityQueue<String>> map;
+    private static final String START="ICN";
+    
+    private static Map<String,PriorityQueue<String>> buildMap(String[][] tickets){
+        Map<String,PriorityQueue<String>> map=new HashMap<>();
+    
+        for(String[] inputs:tickets){
+            map.putIfAbsent(inputs[0],new PriorityQueue<>((a,b)->a.compareTo(b)));
+            map.get(inputs[0]).add(inputs[1]);
+            
+        }
+        
+        return map;
+    }
+
+    private static void DFS(String from){
+        PriorityQueue<String> now=map.get(from);
+        
+        while(now!=null && !now.isEmpty()){
+            String to=now.poll();
+            DFS(to);
+        }
+        
+        result.add(0,from);
+    }
     
     public String[] solution(String[][] tickets) {
+        int size=tickets.length;
+        result=new ArrayList<>();
+        map=buildMap(tickets);
         
-        ticketsCnt=tickets.length;
-        visit=new boolean[ticketsCnt];
+        DFS(START);
         
-        dfs(0,"ICN","ICN",tickets);
-        
-        Collections.sort(answer);
-        String[] str={};
-        
-        str=answer.get(0).split(" ");
-        return str;
-        
+        return result.toArray(new String[0]);
     }
-    private void dfs(int dep,String start,String route,String[][] tickets){
-        if(dep==ticketsCnt){
-            answer.add(route);
-            return;
-        }
-        
-        for(int i=0;i<ticketsCnt;i++){
-            if(tickets[i][0].equals(start) && !visit[i]){
-                visit[i]=true;
-                dfs(dep+1,tickets[i][1],route+" "+tickets[i][1],tickets);
-                visit[i]=false;
-            }
-        }
-    }
+    
 }
