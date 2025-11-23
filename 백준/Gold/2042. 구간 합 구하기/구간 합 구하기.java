7749a8d512bced3877c70ca1c0e47995bd1f8c15
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-23 19:05:58 +0900

    [Gold I] Title: 구간 합 구하기, Time: 500 ms, Memory: 103020 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -4,100 +4,112 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class SegmentTree {
+        int nodes;
+        int height;
+        static long[] tree;
+
+        SegmentTree(int N) {
+            height = (int) Math.ceil(Math.log(N) / Math.log(2));
+            nodes = (int) Math.pow(2, height + 1);
+            tree = new long[nodes];
+        }
+
+        //초기화
+        long init(int start, int end, int node) {
+            if (start == end) {
+                return tree[node] = origin[start];
+            }
+
+            int mid = (start + end) / 2;
+
+            return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+
+        }
+
+        //수정
+        // - newValue:수정하고자 하는 값
+        // -node:수정 인덱스
+        void update(int node, int start, int end, int index, long diff) {
+            if (start > index || end < index) return;
+
+            //해당 값 수정
+            tree[node] += diff;
+            int mid = (start + end) / 2;
+
+            if (start == end) return;
+
+            update(node * 2, start, mid, index, diff);
+            update(node * 2 + 1, mid + 1, end, index, diff);
+        }
+
+        //구간 합 추출
+        long search(int start, int end, int node, int left, int right) {
+            if (left > end || right < start) return 0;
+
+            if (left <= start && right >= end) {
+                return tree[node];
+            }
+
+            int mid = (start + end) / 2;
+
+            return search(start, mid, node * 2, left, right) + search(mid + 1, end, node * 2 + 1, left, right);
+        }
+
+    }
+
     private static int N, M, K;
-    //M : 수 변경 횟수 K:구간 합 구하는 횟수
     private static BufferedReader br;
     private static StringTokenizer st;
-    private static long[] tree;
-    private static int k;
+    private static long[] origin;
+    private static SegmentTree segmentTree;
     private static StringBuilder sb;
-    private static int getCount() {
-        int k = 0;
-        while (true) {
-            k++;
-
-            if (Math.pow(2, k) >= N) break;
-            ;
-        }
-        return k;
-    }
 
     private static void init() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
-        sb=new StringBuilder();
+        sb = new StringBuilder();
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-
-        k = getCount();
-        tree = new long[(int) Math.pow(2, k + 1)];
+        origin = new long[N];
+        segmentTree = new SegmentTree(N);
 
         for (int i = 0; i < N; i++) {
-            tree[i + (int) Math.pow(2, k)] = Long.parseLong(br.readLine());
+            origin[i] = Long.parseLong(br.readLine());
         }
+        segmentTree.init(0, N - 1, 1);
 
-        makeTree();
+    }
+
+    private static void operation() throws IOException {
         for (int i = 0; i < M + K; i++) {
             st = new StringTokenizer(br.readLine());
-            int order = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            long c = Long.parseLong(st.nextToken());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken()) - 1;
 
-            //업데이트
-            if (order == 1) {
-                update(b,c);
+
+            //a=1 => b번째 수를 c로 change
+            if (a == 1) {
+                long c = Long.parseLong(st.nextToken());
+                long diff = c - origin[b];
+                origin[b] = c;
+                segmentTree.update(1, 0, N - 1, b, diff);
             }
-            //부분합
-            else if (order == 2) {
-                sb.append(
-                        interval_sum((int)Math.pow(2,k)+b-1, 
-                                (int) (Math.pow(2,k) + c - 1)
-                        )).append("\n");
+            //a=2 => b번째 수로부터 c번째 수 합
+            else if (a == 2) {
+                int c=Integer.parseInt(st.nextToken());
+                sb.append(segmentTree.search(0, N - 1, 1, b, c - 1)).append("\n");
             }
         }
     }
-    //트리 만들기
-    private static void makeTree(){
-        for (int i=(int)Math.pow(2,k)-1;i>0;i--){
-            tree[i]=tree[i*2]+tree[i*2+1];
-        }
-    }
 
-    //부분합 구하기
-    private static long interval_sum(int start, int end) {
-        long sum=0;
-
-        while (start<=end){
-            if (start%2==1) {
-                sum += tree[start];
-                start++;
-            }
-            if (end%2==0) {
-                sum += tree[end];
-                end--;
-            }
-
-            start/=2;
-            end/=2;
-        }
-        return sum;
-    }
-
-    //업데이트
-    private static void update(int b,long c){
-        int idx=(int) Math.pow(2,k)+b-1;
-        //차이값
-        long diff=c-tree[idx];
-
-        while (idx!=0){
-            tree[idx]+=diff;
-            idx/=2;
-        }
-    }
     public static void main(String[] args) throws IOException {
         init();
+        operation();
+
         System.out.println(sb);
     }
 }
