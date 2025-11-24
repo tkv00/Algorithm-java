8613d5b5769e830015aaf7810eb8fc537e2cfc75
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-24 11:18:00 +0900

    [Gold V] Title: 적록색약, Time: 128 ms, Memory: 15200 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,92 +1,111 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
-    private static char[][] map;
     private static int N;
+    private static BufferedReader br;
+    private static char[][] map;
+    private static final int[] dx = new int[]{0, 0, 1, -1};
+    private static final int[] dy = new int[]{1, -1, 0, 0};
+    private static StringBuilder sb;
 
-    private static Queue<Node> colorQ=new LinkedList<>();
-    private static Queue<Node> nonColorQ=new LinkedList<>();
-    private static int cnt;
-    private static int nonCnt;
-    private static boolean[][] visited;
-    private static boolean[][] visitedNonColor;
-    private static int[] dx=new int[]{0,0,1,-1};
-    private static int[] dy=new int[]{1,-1,0,0};
-    private static boolean isValid(int row,int col){
-        return row>=0 && row<N && col>=0 && col<N ;
-    }
-    private static boolean sameColor(int row,int col,int newRow,int newCol){
-        char nowColor=map[row][col];
-        char nextColor=map[newRow][newCol];
-        return nowColor==nextColor||
-                (nowColor=='R' && nextColor=='G') ||
-                (nowColor=='G' && nextColor=='R');
-    }
-    private static class Node{
-        int row;
-        int col;
-        char color;
-        Node(int row,int col,char color){
-            this.row=row;
-            this.col=col;
-            this.color=color;
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new char[N][N];
+        sb=new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            map[i] = br.readLine().toCharArray();
         }
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        map=new char[N][N];
-        for(int i=0;i<N;i++){
-            String str=br.readLine();
-            map[i]=str.toCharArray();
-        }
-        visited=new boolean[N][N];
-        visitedNonColor=new boolean[N][N];
-        for(int i=0;i<N;i++){
-            for(int j=0;j<N;j++){
-                //적록 색약이 아닌 경우
-                if(!visited[i][j]){
-                    colorQ.offer(new Node(i,j,map[i][j]));
-                    visited[i][j]=true;
-                    while(!colorQ.isEmpty()){
-                        Node now=colorQ.poll();
-                        for(int k=0;k<4;k++){
-                            int newRow=now.row+dx[k];
-                            int newCol=now.col+dy[k];
-                            if(isValid(newRow,newCol)&&
-                                    !visited[newRow][newCol] &&
-                                    (map[newRow][newCol]==map[now.row][now.col])){
-                                colorQ.offer(new Node(newRow,newCol,map[newRow][newCol]));
-                                visited[newRow][newCol]=true;
-                            }
-                        }
-                    }
-                    cnt++;
-                }
 
-                //적록 색약인 경우
-                if(!visitedNonColor[i][j]){
-                    nonColorQ.offer(new Node(i,j,map[i][j]));
-                    visitedNonColor[i][j]=true;
-                    while(!nonColorQ.isEmpty()){
-                        Node now=nonColorQ.poll();
-                        for(int k=0;k<4;k++){
-                            int newRow=now.row+dx[k];
-                            int newCol=now.col+dy[k];
-                            if(isValid(newRow,newCol)&&
-                                !visitedNonColor[newRow][newCol]&&
-                                    sameColor(now.row,now.col,newRow,newCol)
-                            ){
-                                nonColorQ.offer(new Node(newRow,newCol,map[newRow][newCol]));
-                                visitedNonColor[newRow][newCol]=true;
-                            }
+    private static boolean isValid(int row, int col) {
+        return row >= 0 && row < N && col >= 0 && col < N;
+    }
+
+    //적록색약이 아닌 사람
+    private static void isNotBlindness() {
+        int cnt = 0;
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (visited[i][j]) continue;
+                q.offer(new int[]{i, j});
+                visited[i][j] = true;
+                cnt++;
+
+                while (!q.isEmpty()) {
+                    int[] now = q.poll();
+
+                    for (int d = 0; d < 4; d++) {
+                        int nr = now[0] + dx[d];
+                        int nc = now[1] + dy[d];
+
+                        if (isValid(nr, nc) && !visited[nr][nc] && map[nr][nc] == map[i][j]) {
+                            q.offer(new int[]{nr, nc});
+                            visited[nr][nc] = true;
                         }
                     }
-                    nonCnt++;
                 }
             }
         }
-        System.out.print(cnt+" "+nonCnt);
+        sb.append(cnt).append(" ");
+    }
 
+    //적록색약인 사람
+    private static void blindness(){
+        int cnt=0;
+        Queue<Object[]> q=new LinkedList<>();
+        boolean[][] visited=new boolean[N][N];
+
+        for (int i=0;i<N;i++){
+            for (int j=0;j<N;j++){
+                if (visited[i][j])continue;
+
+                q.offer(new Object[]{i,j,map[i][j]});
+                visited[i][j]=true;
+                cnt++;
+
+                while (!q.isEmpty()){
+                    Object[] now=q.poll();
+
+                    for (int d=0;d<4;d++){
+                        int nr=(int)now[0]+dx[d];
+                        int nc=(int)now[1]+dy[d];
+
+                        if(isValid(nr,nc) && !visited[nr][nc]){
+                            //빨강 혹은 초록색인 경우 => 같은 영역
+                            if ((char)now[2] == 'R' || (char) now[2]=='G'){
+                                if (map[nr][nc]=='B') continue;
+                                q.offer(new Object[]{nr,nc,map[nr][nc]});
+                                visited[nr][nc]=true;
+                            }
+                            else{
+                                if (map[nr][nc]=='B'){
+                                    visited[nr][nc]=true;
+                                    q.offer(new Object[]{nr,nc,map[nr][nc]});
+                                }
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+
+        sb.append(cnt);
+    }
+    public static void main(String[] args) throws IOException {
+        init();
+        isNotBlindness();
+        blindness();
+
+        System.out.println(sb);
     }
 }
