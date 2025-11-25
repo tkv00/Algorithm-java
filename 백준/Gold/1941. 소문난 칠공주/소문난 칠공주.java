79ec66f48ff0685deb946b14fd40e1a15a7c49b9
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-25 10:55:54 +0900

    [Gold III] Title: 소문난 칠공주, Time: 444 ms, Memory: 155404 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.java
@@ -1,104 +1,98 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 
 public class Main {
-    static class Point {
-        int row;
-        int col;
+    private static final int SIZE = 5;
+    private static int[] dx = new int[]{0, 0, 1, -1};
+    private static int[] dy = new int[]{1, -1, 0, 0};
+    private static char[][] map;
+    private static BufferedReader br;
+    private static int result = 0;
+    private static boolean[] selected;
 
-        Point(int row, int col) {
-            this.row = row;
-            this.col = col;
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        map = new char[SIZE][SIZE];
+        selected = new boolean[SIZE * SIZE];
+
+        for (int i = 0; i < SIZE; i++) {
+            String input = br.readLine();
+            map[i] = input.toCharArray();
         }
+
     }
 
-    static char[][] map;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-    static boolean[][] visited = new boolean[5][5];
-    static int res = 0;
+    private static boolean isValid(int row, int col) {
+        return row >= 0 && row < SIZE && col >= 0 && col < SIZE;
+    }
 
+    private static boolean BFS(boolean[] selected) {
+        Queue<int[]> q = new LinkedList<>();
+        boolean[][] visited = new boolean[SIZE][SIZE];
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        map = new char[5][5];
-        for (int i = 0; i < 5; i++) {
-            String str = br.readLine();
-            char[] str2 = str.toCharArray();
-            for (int j = 0; j < 5; j++) {
-                map[i][j] = str2[j];
+        for (int i = 0; i < SIZE * SIZE; i++) {
+            if (selected[i]) {
+                int row = i / SIZE;
+                int col = i % SIZE;
+                visited[row][col] = true;
+                q.offer(new int[]{row, col});
+                break;
             }
         }
-        dfs(0, 0, 0);
-        //조건 1.s가 4명이상
-        // 조건 2. 7명 서로 인접
+        int cnt = 0;
 
-        System.out.println(res);
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            cnt++;
 
-    }
+            for (int d = 0; d < 4; d++) {
+                int nr = now[0] + dx[d];
+                int nc = now[1] + dy[d];
 
-    static void dfs(int n, int cnt, int scnt) {
-        if (cnt > 7) return;
-        if (n == 25) {
-            if (cnt == 7 && scnt >= 4) {
-                //7명이 인접한 지 체크
-                if (check()) {
-                    res += 1;
-                }
-            }
-            return;
+                if (!isValid(nr, nc) || visited[nr][nc]) continue;
 
-        }
-        int row = n / 5;
-        int col = n % 5;
-
-        visited[row][col] = true;
-        //포함할때
-        dfs(n + 1, cnt + 1, scnt + (map[row][col] == 'S' ? 1 : 0));
-        visited[row][col] = false;
-
-        //포함하지 않을때
-        dfs(n + 1, cnt, scnt);
-    }
-
-    static boolean check() {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                //방문을 한 곳이라면
-                if (visited[i][j]) {
-                    return bfs(i, j);
+                if (selected[nr * SIZE + nc]) {
+                    q.offer(new int[]{nr, nc});
+                    visited[nr][nc] = true;
                 }
             }
         }
-        return false;
-    }
 
-    static boolean bfs(int startRow, int startCol) {
-        Queue<Point> queue = new LinkedList<>();
-        //방문된 곳 탐색
-        boolean[][] visit = new boolean[5][5];
-        queue.add(new Point(startRow, startCol));
-        visit[startRow][startCol] = true;
-        int cnt = 1;
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int newRow = point.row + dx[i];
-                int newCol = point.col + dy[i];
-                //경계 처리
-                if ((newRow >= 0 && newRow < 5) && (newCol >= 0 && newCol < 5) && !visit[newRow][newCol]&&visited[newRow][newCol]) {
-                    visit[newRow][newCol] = true;
-                    cnt += 1;
-                    queue.add(new Point(newRow, newCol));
-                }
-            }
-        }
         return cnt == 7;
     }
 
-}
+    private static void combination(int count, int num, int idx, boolean[] selected) {
+     
+        if (count == 7) {
+            if (num >= 4) {
+                if (BFS(selected)) result++;
+            }
+            return;
+        }
 
+        if (idx >= SIZE * SIZE) return;
+        
+        int row = idx / SIZE;
+        int col = idx % SIZE;
+
+
+        //선택하는 경우
+        selected[idx] = true;
+        combination(count + 1, map[row][col] == 'S' ? num + 1 : num, idx + 1, selected);
+
+        //선택하지 않는 경우
+        selected[idx] = false;
+        combination(count, num, idx + 1, selected);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        combination(0, 0, 0, selected);
+
+        System.out.println(result);
+    }
+}
