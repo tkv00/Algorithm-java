5e4ad7b5d536b671c08a83339bee7a6e334f2a01
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-25 10:08:52 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 788 ms, Memory: 176752 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -10,11 +10,11 @@ public class Main {
     private static StringTokenizer st;
     private static BufferedReader br;
     private static int[][] map;
+    private static int[] dx = new int[]{0, 0, 1, -1};
+    private static int[] dy = new int[]{1, -1, 0, 0};
+    private static Queue<int[]> q;
     private static int[][][] distMap;
     private static boolean[][][] visited;
-    private static Queue<int[]> q;
-    private static int[] dr = new int[]{0, 0, 1, -1};
-    private static int[] dc = new int[]{1, -1, 0, 0};
 
     private static void init() throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -24,60 +24,66 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
 
         map = new int[N][M];
-        visited = new boolean[N][M][2];
         distMap = new int[N][M][2];
+        visited = new boolean[N][M][2];
+
         q = new LinkedList<>();
 
         for (int i = 0; i < N; i++) {
             String input = br.readLine();
-            char[] chArr = input.toCharArray();
             for (int j = 0; j < M; j++) {
-                map[i][j] = chArr[j] - '0';
+                map[i][j] = input.charAt(j) - '0';
             }
         }
     }
 
-    private static boolean isValid(int row, int col, int w) {
-        return row >= 0 && row < N && col >= 0 && col < M && !visited[row][col][w];
+    private static boolean isValid(int row, int col) {
+        return row >= 0 && row < N && col >= 0 && col < M;
     }
 
-
     private static void BFS() {
+        q.offer(new int[]{0, 0, 0});
         visited[0][0][0] = true;
         distMap[0][0][0] = 1;
-        q.offer(new int[]{0, 0, 0});
+        distMap[0][0][1] = 1;
 
         while (!q.isEmpty()) {
             int[] now = q.poll();
 
-            if (now[0] == N - 1 && now[1] == M - 1) {
-                System.out.println(distMap[now[0]][now[1]][now[2]]);
-                System.exit(0);
-            }
             for (int d = 0; d < 4; d++) {
-                int nr = now[0] + dr[d];
-                int nc = now[1] + dc[d];
+                int nr = now[0] + dx[d];
+                int nc = now[1] + dy[d];
 
-                if (!isValid(nr, nc, now[2])) continue;
-                //벽인 경우
-                if (map[nr][nc] == 1 && now[2] == 0) {
-                    //이전 값이 벽을
-                    visited[nr][nc][1] = true;
-                    distMap[nr][nc][1] = distMap[now[0]][now[1]][0] + 1;
-                    q.offer(new int[]{nr, nc, 1});
-                } else if (map[nr][nc] == 0) {
-                    visited[nr][nc][now[2]] = true;
-                    distMap[nr][nc][now[2]] = distMap[now[0]][now[1]][now[2]] + 1;
-                    q.offer(new int[]{nr, nc, now[2]});
-                }
+                if (!isValid(nr, nc) || visited[nr][nc][now[2]]) continue;
+                if (now[2] == 1 && map[nr][nc] == 1) continue;
 
+                visited[nr][nc][now[2]] = true;
+                distMap[nr][nc][now[2]+map[nr][nc]] = distMap[now[0]][now[1]][now[2]] + 1;
+                q.offer(new int[]{nr, nc, now[2] + map[nr][nc]});
             }
         }
+
     }
 
     public static void main(String[] args) throws IOException {
         init();
         BFS();
-        System.out.println(-1);
+
+        if (distMap[N - 1][M - 1][0] == 0 && distMap[N - 1][M - 1][1] == 0) {
+            System.out.println(-1);
+            return;
+        }
+
+        if (distMap[N - 1][M - 1][0] == 0) {
+            System.out.println(distMap[N - 1][M - 1][1]);
+            return;
+        }
+
+        if (distMap[N - 1][M - 1][1] == 0) {
+            System.out.println(distMap[N - 1][M - 1][0]);
+            return;
+        }
+
+        System.out.println(Math.min(distMap[N - 1][M - 1][0], distMap[N - 1][M - 1][1]));
     }
 }
