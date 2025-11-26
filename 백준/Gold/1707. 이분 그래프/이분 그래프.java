b9beebdca1e852c002dc904fa81abb15948365e6
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-26 10:52:36 +0900

    [Gold IV] Title: 이분 그래프, Time: 832 ms, Memory: 267804 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -1,84 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
-import static java.lang.System.exit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static int T;
-    public static int V;
-    public static int E;
-    public static final int RED = 1;
-    public static final int BLUE = -1;
-    public static StringTokenizer st;
-    public static ArrayList<Integer>[] arrayLists;
-    public static int[] colors;
+    private static int K;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static List<Integer>[] graph;
+    private static int[] colors;
+    private static int V,E;
+    private static StringBuilder sb;
+    private static boolean flag;
+    private static void init() throws IOException {
+        br=new BufferedReader(new InputStreamReader(System.in));
+        K=Integer.parseInt(br.readLine());
+        sb=new StringBuilder();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        st = new StringTokenizer(br.readLine());
-        T = Integer.parseInt(st.nextToken());
+        for (int i=0;i<K;i++){
+            flag=false;
+            st=new StringTokenizer(br.readLine());
+            V=Integer.parseInt(st.nextToken());
+            E=Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < T; i++) {
-            st = new StringTokenizer(br.readLine());
-            V = Integer.parseInt(st.nextToken());
-            E = Integer.parseInt(st.nextToken());
-            arrayLists = new ArrayList[V + 1];
-            colors = new int[V + 1];
-            //리스트 초기화
-            for (int j = 0; j <= V; j++) {
-                arrayLists[j] = new ArrayList<>();
-            }
+            colors=new int[V+1]; //0:미방문 1:Red -1:Blue
+            graphInit(V,E);
 
-            //입력값 받기
-            for (int k = 0; k < E; k++) {
-                st = new StringTokenizer(br.readLine());
-                int start = Integer.parseInt(st.nextToken());
-                int end = Integer.parseInt(st.nextToken());
-                arrayLists[start].add(end);
-                arrayLists[end].add(start);
-            }
-
-            boolean res = false;
-            for (int t = 1; t <= V; t++) {
-                if (colors[t] == 0) {
-                    res = bfs(t, RED);
+            flag=false;
+            for (int v=1;v<=V;v++){
+                if (colors[v]==0){
+                    if (!DFS(v,1)) {
+                        flag = true;
+                        break;
+                    }
                 }
-                if (!res) break;
-            }
-            if (!res) {
-                System.out.println("NO");
-            } else {
-                System.out.println("YES");
             }
 
+            sb.append(flag ? "NO" : "YES").append("\n");
         }
-        //이분 그래프가 아닌 경우
-
-
     }
 
-    public static boolean bfs(int start, int color) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        colors[start] = color;//일단 빨간색 색칠
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            for (int v : arrayLists[now]) {
-                //같은 색깔이면  이분 그래프가 아님
-                if (colors[v] == colors[now]) {
-                    return false;
-                }
-                if (colors[v] == 0) {
-                    queue.add(v);
-                    colors[v] = colors[now] * -1;
-                }
-            }
+    private static void graphInit(int v,int e) throws IOException {
+        graph=new List[v+1];
+        for (int i=1;i<=v;i++){
+            graph[i]=new ArrayList<>();
         }
+
+        for (int i=0;i<e;i++){
+            st=new StringTokenizer(br.readLine());
+
+            int node1=Integer.parseInt(st.nextToken());
+            int node2=Integer.parseInt(st.nextToken());
+
+            graph[node1].add(node2);
+            graph[node2].add(node1);
+        }
+    }
+
+    private static boolean DFS(int now,int prevColor){
+        colors[now]=prevColor;
+
+        for (int next:graph[now]){
+            //방문 하지 않은 경우
+            if (colors[next]==0){
+                if (!DFS(next,-prevColor)) return false;
+            }
+            //방문 한 경우 => 색깔 비교
+            else if (colors[next]==prevColor) return false;
+        }
+
         return true;
     }
-
+    public static void main(String[] args) throws IOException {
+        init();
+        System.out.println(sb);
+    }
 }
-
-
