c767f397f556fdbe9eccf853b981ed26e9f61b48
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-11-27 11:25:42 +0900

    [Gold III] Title: 개미굴, Time: 152 ms, Memory: 15816 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -1,73 +1,63 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
-    private static BufferedReader br;
-    private static StringTokenizer st;
     private static int N;
+    private static StringTokenizer st;
+    private static BufferedReader br;
     private static StringBuilder sb;
+    private static final String FLOOR="--";
     private static Trie trie;
-    private static class Node{
-        TreeMap<String,Node> child=new TreeMap<>((a,b)->a.compareTo(b));
-    }
-
     private static class Trie{
-        Node root=new Node();
+       TreeMap<String,Trie> child=new TreeMap<>((a,b)->a.compareTo(b));
 
-        void insert(String str){
-            Node node=this.root;
+        void insert(String[] inputs){
+            Trie root=this;
 
-            String[] strings=str.split(" ");
-            int num=Integer.parseInt(strings[0]);
-
-            for (int i=1;i<=num;i++){
-                node.child.putIfAbsent(strings[i],new Node());
-                node=node.child.get(strings[i]);
-            }
-        }
-
-        void search(int depth,Node node){
-            TreeMap<String,Node> children=node.child;
-            if (children==null) return;
-
-            Set<Character> set=new HashSet<>();
-            for (String next:children.keySet()){
-
-
-                for (int i=0;i<depth;i++){
-                    sb.append("--");
-                }
-                sb.append(next).append("\n");
-
-                search(depth+1,node.child.get(next));
+            for (String input:inputs){
+                root.child.putIfAbsent(input,new Trie());
+                root=root.child.get(input);
             }
         }
     }
-
     private static void init() throws IOException {
-        sb=new StringBuilder();
         br=new BufferedReader(new InputStreamReader(System.in));
-
         N=Integer.parseInt(br.readLine());
         trie=new Trie();
+        sb=new StringBuilder();
 
         for (int i=0;i<N;i++){
-            String input=br.readLine();
-            trie.insert(input);
-        }
+            st=new StringTokenizer(br.readLine());
+            int K=Integer.parseInt(st.nextToken());
+            String[] inputs=new String[K];
 
+            for (int k=0;k<K;k++){
+                inputs[k]=st.nextToken();
+            }
+            trie.insert(inputs);
+        }
     }
-    private static void operation(){
-        trie.search(0,trie.root);
-        System.out.println(sb);
+
+    private static void DFS(Trie now,int depth){
+       for (Map.Entry<String,Trie> next:now.child.entrySet()){
+            for (int i=0;i<depth;i++){
+                sb.append(FLOOR);
+            }
+
+            sb.append(next.getKey()).append("\n");
+
+            DFS(next.getValue(),depth+1);
+       }
     }
+
     public static void main(String[] args) throws IOException {
         init();
-        operation();
+        DFS(trie,0);
+
+        System.out.println(sb);
     }
 }
