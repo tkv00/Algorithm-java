413d11b86bc7fab7501969ee1577b71b67322b2a
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-02 11:28:59 +0900

    [Gold III] Title: 줄 세우기, Time: 456 ms, Memory: 49816 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,60 +4,60 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static int N;
-    public static int M;
-    public static StringTokenizer st;
-    public static ArrayList<Integer>[] arr;
-    public static int[] indegree;
-    public static Stack<Integer> stack;
-    public static int []res;
-    public static int index=0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    private static int N,M;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static int[] indegree;
+    private static StringBuilder sb;
+    private static Map<Integer, List<Integer>> map;
+    private static void init() throws IOException {
+        br=new BufferedReader(new InputStreamReader(System.in));
         st=new StringTokenizer(br.readLine());
+        map=new HashMap<>();
+        sb=new StringBuilder();
+
         N=Integer.parseInt(st.nextToken());
         M=Integer.parseInt(st.nextToken());
-        arr=new ArrayList[N+1];
-        res=new int[N];
-        for(int i=0;i<=N;i++){
-            arr[i]=new ArrayList<>();
-        }
+
         indegree=new int[N+1];
-        for(int i=0;i<M;i++){
+
+        for (int i=0;i<M;i++){
             st=new StringTokenizer(br.readLine());
-            int a=Integer.parseInt(st.nextToken());
-            int b=Integer.parseInt(st.nextToken());
-            arr[a].add(b);
-            indegree[b]++;
+
+            int studentA=Integer.parseInt(st.nextToken());
+            int studentB=Integer.parseInt(st.nextToken());
+
+            indegree[studentB]++;
+            map.putIfAbsent(studentA,new ArrayList<>());
+            map.get(studentA).add(studentB);
         }
-       // System.out.println(Arrays.toString(arr));
-        //System.out.println("indegree"+Arrays.toString(indegree));
-        stack=new Stack<>();
-        for(int i=1;i<=N;i++){
-            if(indegree[i]==0){
-                stack.push(i);
-            }
-           // System.out.println("stack"+stack);
-        }
-        bfs();
-        for(int i=0;i<N;i++){
-            System.out.print(res[i]+" ");
+    }
+
+    private static void operation(){
+        Queue<Integer> q=new LinkedList<>();
+
+        for (int i=1;i<=N;i++){
+            if (indegree[i]==0) q.offer(i);
         }
 
-    }
-    public static void bfs(){
-        while (!stack.isEmpty()){
-            int v=stack.pop();
-           // System.out.println(v);
-            res[index++]=v;
-            for(int a:arr[v]){
-                indegree[a]--;
-               // System.out.println("indegree[a]"+ Arrays.toString(indegree));
-                if(indegree[a]==0){
-                    stack.push(a);
+        while(!q.isEmpty()){
+            int now=q.poll();
+            sb.append(now).append(" ");
+
+            if (!map.containsKey(now)) continue;
+
+            for (int next:map.get(now)){
+                indegree[next]--;
+                if (indegree[next]==0){
+                    q.offer(next);
                 }
             }
         }
     }
+    public static void main(String[] args) throws IOException {
+        init();
+        operation();
 
+        System.out.println(sb);
+    }
 }
