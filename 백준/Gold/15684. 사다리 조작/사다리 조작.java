c415715cc0a75e5a3c995aaa8e996b3cd7ecf481
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-03 11:02:06 +0900

    [Gold III] Title: 사다리 조작, Time: 2928 ms, Memory: 18504 KB -BaekjoonHub

--- 백준/Gold/15684. 사다리 조작/사다리 조작.java
+++ 백준/Gold/15684. 사다리 조작/사다리 조작.java
@@ -1,100 +1,112 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static int N,M;//N : 세로선  M : 가로선
-    private static int H;
-    private static BufferedReader br;
+    private static int N, M, H;
     private static StringTokenizer st;
-    private static int[] nowArr;
-    private static int[][] map;
-    private static int min=Integer.MAX_VALUE;
+    private static BufferedReader br;
+    private static boolean[][] map;
+    private static int[] endPoint;
+    private static final int MAX = 3;
+    private static int result = -1;
+
     private static void init() throws IOException {
-        br=new BufferedReader(new InputStreamReader(System.in));
-        String input=br.readLine();
-        N=Integer.parseInt(input.split(" ")[0]);
-        M=Integer.parseInt(input.split(" ")[1]);
-        H=Integer.parseInt(input.split(" ")[2]);
-        map=new int[H+1][N+1];
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
-        for(int i=0;i<M;i++){
-            st=new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
 
-            //가로선 정보
-            int a=Integer.parseInt(st.nextToken());
-            //연결 번호
-            int b=Integer.parseInt(st.nextToken());
-            //왼쪽->오른쪽 연결
-            map[a][b]=1;
-            //오른쪽->왼쪽 연결
-            map[a][b+1]=2;
+        map = new boolean[H + 1][N + 1];
+        endPoint = new int[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int h = Integer.parseInt(st.nextToken());//깊이 정보
+            int n = Integer.parseInt(st.nextToken());//가로 시작 위치
+
+            map[h][n] = true;
         }
-
     }
-    //사다리 타기
-    private static boolean simulation(){
-        nowArr=new int[N+1];
-        for(int i=1;i<=N;i++){
-            nowArr[i]=i;
-        }
-        //1번부터 N번까지 차례대로 사다리 타기 시작.
-        for(int i=1;i<=H;i++){
-            for(int j=1;j<=N;j++){
-                //left -> right
-                if(map[i][j]==1){
-                    int temp=nowArr[j];
-                    nowArr[j]=nowArr[j+1];
-                    nowArr[j+1]=temp;
-                }
+
+    //각 번호별 도착 위치 찾기
+    private static void getEndPoint(int start) {
+        int tmp = start;
+
+        for (int row = 1; row <= H; row++) {
+            if (tmp < N && map[row][tmp]) {
+                tmp++;
+            } else if (tmp > 1 && map[row][tmp - 1]) {
+                tmp--;
             }
         }
 
-        return isEnd(nowArr);
+        endPoint[start] = tmp;
     }
 
-    private static boolean isEnd(int[] arr){
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]!=i) return false;
+    //모든 번호들에 대해서 도착 위치 찾기
+    private static void getAllEndPoint() {
+        for (int start = 1; start <= N; start++) {
+            getEndPoint(start);
         }
+    }
+
+    //사다리 조작 여부
+    private static boolean isFinished() {
+        getAllEndPoint();
+
+        for (int i = 1; i <= N; i++) {
+            if (endPoint[i] != i) return false;
+        }
+
         return true;
     }
 
-    //DFS
-    private static void DFS(int cnt,int row,int col){
-        if(cnt>=min) return;
-        //중단 조건 : 다 제자리로 도착하는 경우
-        if(simulation()){
-            min=cnt;
+    private static boolean canPut(int h, int n) {
+        if (map[h][n]) return false;
+        if (n > 1 && map[h][n - 1]) return false;
+        if (n < N && map[h][n + 1]) return false;
+        return true;
+    }
+
+    private static void dfs(int cnt, int row, int col) {
+        if (result != -1 && result <= cnt) return;
+
+        if (cnt > MAX) {
             return;
         }
 
-        if(cnt==3){
+        if (isFinished()) {
+            result = cnt;
             return;
         }
 
+        for (int h = row; h <= H; h++) {
+            for (int n = (h == row ? col : 1); n < N; n++) {
+                if (!canPut(h, n)) continue;
 
-        for(int i=row;i<=H;i++){
-            for(int j=(i==row ? col : 1);j<N;j++){
-                if(map[i][j]==0 && map[i][j+1]==0){
-                    map[i][j]=1;
-                    map[i][j+1]=2;
-                    DFS(cnt+1,i,j+2);
-                    map[i][j]=0;
-                    map[i][j+1]=0;
-                }
+                map[h][n] = true;
+
+                dfs(cnt + 1, h, n);
+
+                map[h][n] = false;
+
+                if (result == cnt + 1) return;
             }
-
         }
-
 
     }
+
+
     public static void main(String[] args) throws IOException {
         init();
-        simulation();
-        DFS(0,1,1);
+        dfs(0, 1, 1);
 
-        System.out.print(min == Integer.MAX_VALUE ? -1 : min);
+        System.out.println(result);
     }
 }
+
