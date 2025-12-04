cdc95c53c8695ee51635a89a6be107a2566bebcd
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-04 09:38:36 +0900

    [Gold IV] Title: 플로이드, Time: 360 ms, Memory: 42572 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,48 +1,68 @@
-import java.io.*;
-import java.util.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class Main {
-    private static int N,M;
-    private static int[][] map;
+    private static int n,m;
     private static StringTokenizer st;
-    private static final int INF=1000000001;
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        M=Integer.parseInt(br.readLine());
-        map=new int[N+1][N+1];
-        for(int i=1;i<=N;i++){
-            Arrays.fill(map[i],INF);
+    private static BufferedReader br;
+    private static final int MAX_VALUE=10_000_001;
+    private static int[][] map;
+    private static StringBuilder sb;
+    private static void init() throws IOException {
+        br=new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(br.readLine());
+        m=Integer.parseInt(br.readLine());
+        sb=new StringBuilder();
+
+        map=new int[n+1][n+1];
+
+        for (int i=1;i<=n;i++){
+            Arrays.fill(map[i],MAX_VALUE);
             map[i][i]=0;
         }
-        for(int i=0;i<M;i++){
+
+        for (int i=0;i<m;i++){
             st=new StringTokenizer(br.readLine());
-            int start=Integer.parseInt(st.nextToken());
-            int end=Integer.parseInt(st.nextToken());
+            int city_1=Integer.parseInt(st.nextToken());
+            int city_2=Integer.parseInt(st.nextToken());
             int cost=Integer.parseInt(st.nextToken());
 
-            map[start][end]=Math.min(map[start][end],cost);
+            map[city_1][city_2]=Math.min(cost,map[city_1][city_2]);
         }
+    }
 
-        for(int i=1;i<=N;i++){
-            for(int j=1;j<=N;j++){
-                for(int k=1;k<=N;k++){
-                    map[j][k]=Math.min(map[j][k],map[j][i]+map[i][k]);
+    private static void operation(){
+        for (int k=1;k<=n;k++){
+            for (int i=1;i<=n;i++){
+                for (int j=1;j<=n;j++){
+                    if (i==j) continue;
+                    map[i][j]=Math.min(map[i][k]+map[k][j],map[i][j]);
                 }
             }
         }
+    }
 
-        StringBuilder sb=new StringBuilder();
-               for(int i=1;i<=N;i++){
-            for(int j=1;j<=N;j++){
-                if(map[i][j]==INF){
+    private static void printMap(){
+        for (int i=1;i<=n;i++){
+            for (int j=1;j<=n;j++){
+                if (map[i][j]==MAX_VALUE){
                     sb.append(0).append(" ");
-                }else{
-                    sb.append(map[i][j]).append(" ");
+                    continue;
                 }
+                sb.append(map[i][j]).append(" ");
             }
             sb.append("\n");
         }
 
-        System.out.print(sb);
+        System.out.println(sb);
+    }
+    public static void main(String[] args) throws IOException {
+        init();
+        operation();
+        printMap();
     }
 }
