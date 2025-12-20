bc760d9aadf4e98593c961fb8e84a861d3a6ca68
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-20 17:44:59 +0900

    [Gold V] Title: LCS, Time: 120 ms, Memory: 18204 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -1,37 +1,41 @@
+
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static String str1;
-    public static String str2;
-    public static char[]arr1;
-    public static char[]arr2;
-    public static int[][] LCS;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        str1=br.readLine();
-        str2=br.readLine();
-        arr1=str1.toCharArray();
-        arr2=str2.toCharArray();
-        int row=arr1.length;
-        int col=arr2.length;
+    private static BufferedReader br;
+    private static char[] input1;
+    private static char[] input2;
+    private static int[][] dp;
+    private static int row, col;
 
-        LCS=new int[row+1][col+1];
-        int max=-1;
-        for(int i=1;i<=col;i++){
-            for(int j=1;j<=row;j++){
-                //같은 경우
-                if(arr1[j-1]==arr2[i-1]){
-                    LCS[j][i]=LCS[j-1][i-1]+1;
-                }else{
-                    LCS[j][i]=Math.max(LCS[j-1][i],LCS[j][i-1]);
-                }
-                max=Math.max(max,LCS[j][i]);
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+
+        input1 = br.readLine().toCharArray();
+        input2 = br.readLine().toCharArray();
+
+        row = input1.length;
+        col = input2.length;
+
+        dp = new int[row + 1][col + 1];
+    }
+
+    private static void dynamicProgramming() {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (input1[i-1] == input2[j-1]) dp[i][j] = dp[i - 1][j - 1] + 1;
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
             }
         }
-        System.out.println(max);
+    }
 
+    public static void main(String[] args) throws IOException {
+        init();
+        dynamicProgramming();
 
+        System.out.println(dp[row][col]);
     }
 }
