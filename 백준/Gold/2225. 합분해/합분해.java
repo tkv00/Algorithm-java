ef4fd46ca90376459736ef266d6e51c4871177f7
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-20 19:01:40 +0900

    [Gold V] Title: 합분해, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,39 +1,42 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int col;
-    static int row;
-    static int res=0;
-    static long[][]dp;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static int N, K;
+    private static final int MOD = 1_000_000_000;
+    private static int[][] dp;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st=new StringTokenizer(br.readLine());
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
 
-        col=Integer.parseInt(st.nextToken());
-        row=Integer.parseInt(st.nextToken());
-        dp=new long[row+1][col+1];
-        for(int i=0;i<=col;i++){
-            dp[1][i]=1;
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        dp = new int[N + 1][K + 1];
+
+        for (int i = 1; i <= K; i++) {
+            dp[0][i] = 1;
         }
-        for(int j=0;j<=row;j++){
-            dp[j][0]=1;
-        }
-        for(int i=2;i<=row;i++){
-            for(int j=1;j<=col;j++){
-                dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                dp[i][j]%=1000000000;
-            }
-        }
-        System.out.println(dp[row][col]);
-
-
-
-
-
     }
 
+    private static void dynamicProgramming() {
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= K; j++) {
+                dp[i][j] = (dp[i - 1][j] % MOD + dp[i][j - 1] % MOD) % MOD;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        dynamicProgramming();
+
+        System.out.println(dp[N][K] % MOD);
+    }
 }
