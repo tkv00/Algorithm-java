5e759b46057605ed9d3a1ee21e9becc6c396d989
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-21 17:15:18 +0900

    [Gold IV] Title: 동전 1, Time: 104 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,40 +6,41 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static BufferedReader br;
+    private static int n, k;
     private static StringTokenizer st;
-    private static int n,k;
-    private static int[] coins;
+    private static BufferedReader br;
     private static int[] dp;
+    private static int[] inputs;
+
     private static void init() throws IOException {
-        br=new BufferedReader(new InputStreamReader(System.in));
-        st=new StringTokenizer(br.readLine());
-        n=Integer.parseInt(st.nextToken());
-        k=Integer.parseInt(st.nextToken());
+        br = new BufferedReader(new InputStreamReader(System.in));
+        st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
 
-        dp=new int[k+1];
-        coins=new int[n];
-        dp[0]=1;
+        inputs = new int[n];
+        dp = new int[k + 1];
+        dp[0] = 1;
 
-        for (int i=0;i<n;i++){
-            coins[i]=Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            inputs[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(coins);
+        Arrays.sort(inputs);
     }
 
-    private static void dynamicProgramming(){
-        for (int coin=0;coin<n;coin++){
-            for (int j=coins[coin];j<=k;j++){
-                dp[j]+=dp[j-coins[coin]];
+    private static void operation() {
+        for (int i = 0; i < n; i++) {
+            for (int money = inputs[i]; money <= k; money++) {
+                dp[money]+=dp[money-inputs[i]];
             }
         }
     }
 
-
     public static void main(String[] args) throws IOException {
         init();
-        dynamicProgramming();
+        operation();
+
         System.out.println(dp[k]);
     }
 }
