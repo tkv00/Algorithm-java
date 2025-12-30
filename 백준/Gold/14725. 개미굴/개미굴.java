1289d83509130288e0dcc8293ac2fcaf6481a70f
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2025-12-30 15:26:12 +0900

    [Gold III] Title: 개미굴, Time: 144 ms, Memory: 16132 KB -BaekjoonHub

--- 백준/Gold/14725. 개미굴/개미굴.java
+++ 백준/Gold/14725. 개미굴/개미굴.java
@@ -1,21 +1,21 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
 
 public class Main {
-    private static int N;
-    private static StringTokenizer st;
-    private static BufferedReader br;
-    private static StringBuilder sb;
-    private static final String FLOOR="--";
-    private static Trie trie;
-    private static class Trie{
-       TreeMap<String,Trie> child=new TreeMap<>((a,b)->a.compareTo(b));
+    private static class Trie {
+        String name;
+        TreeMap<String, Trie> child = new TreeMap<>((a,b)->a.compareTo(b));
 
-        void insert(String[] inputs){
+        public Trie( ) {
+        }
+
+        private void insert(String[] inputs){
             Trie root=this;
 
             for (String input:inputs){
@@ -24,39 +24,41 @@ public class Main {
             }
         }
     }
+
+    private static int N, K;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static final String PARTITION = "--";
+    private static StringBuilder sb;
+    private static Trie trie;
+
     private static void init() throws IOException {
-        br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
         trie=new Trie();
-        sb=new StringBuilder();
+        sb = new StringBuilder();
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        for (int i=0;i<N;i++){
-            st=new StringTokenizer(br.readLine());
-            int K=Integer.parseInt(st.nextToken());
-            String[] inputs=new String[K];
-
-            for (int k=0;k<K;k++){
-                inputs[k]=st.nextToken();
-            }
-            trie.insert(inputs);
+        for (int i = 0; i < N; i++) {
+            String[] inputs = br.readLine().split(" ");
+            String[] cleaned=Arrays.copyOfRange(inputs,1,inputs.length);
+            trie.insert(cleaned);
         }
     }
 
-    private static void DFS(Trie now,int depth){
-       for (Map.Entry<String,Trie> next:now.child.entrySet()){
+    private static void dfs(Trie now,int depth) {
+        for (Map.Entry<String,Trie> next:now.child.entrySet()){
             for (int i=0;i<depth;i++){
-                sb.append(FLOOR);
+                sb.append(PARTITION);
             }
 
             sb.append(next.getKey()).append("\n");
-
-            DFS(next.getValue(),depth+1);
-       }
+            dfs(next.getValue(),depth+1);
+        }
     }
 
     public static void main(String[] args) throws IOException {
         init();
-        DFS(trie,0);
+        dfs(trie,0);
 
         System.out.println(sb);
     }
