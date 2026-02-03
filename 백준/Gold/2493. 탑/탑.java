03d13c1ce3d98f665c6980d5df9b7a2de85ba5fd
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2026-02-03 19:48:22 +0900

    [Gold V] Title: 탑, Time: 900 ms, Memory: 90324 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,43 +1,67 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.*;
+import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-    private static class Node{
-        int data;
+    private static Block[] inputs;
+    private static StringBuilder sb;
+
+    private static class Block {
         int idx;
-        Node(int data,int idx){
-            this.data=data;
-            this.idx=idx;
+        int h;
+
+        Block(int idx, int h) {
+            this.h = h;
+            this.idx = idx;
         }
     }
-    private static Stack<Node> stack=new Stack<>();
-    private static StringTokenizer st;
+
     private static int N;
+    private static BufferedReader br;
+    private static StringTokenizer st;
+    private static Stack<Block> stack;
+
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        sb = new StringBuilder();
+        stack = new Stack<>();
+        inputs = new Block[N + 1];
+        st = new StringTokenizer(br.readLine());
+
+        for (int idx = 1; idx <= N; idx++) {
+            inputs[idx] = new Block(idx, Integer.parseInt(st.nextToken()));
+        }
+    }
+
+    private static void operation() {
+        for (int i = 1; i <= N; i++) {
+            Block now = inputs[i];
+
+            while (!stack.isEmpty()) {
+                if (stack.peek().h > now.h){
+                    sb.append(stack.peek().idx).append(" ");
+                    stack.push(now);
+                    break;
+                }
+                if (stack.peek().h < now.h) {
+                    stack.pop();
+                }
+            }
+            if (stack.isEmpty()) {
+                stack.push(now);
+                sb.append(0).append(" ");
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        N=Integer.parseInt(br.readLine());
-        st=new StringTokenizer(br.readLine());
-        StringBuilder sb=new StringBuilder();
+        init();
+        operation();
 
-        for(int i=1;i<=N;i++){
-            int height=Integer.parseInt(st.nextToken());
-            while(!stack.isEmpty() && stack.peek().data<height){
-                stack.pop();
-            }
-            if(stack.isEmpty()){
-                sb.append("0 ");
-            }else{
-                sb.append(stack.peek().idx+" ");
-            }
-            stack.push(new Node(height,i));
-
-        }
-        System.out.print(sb);
-
+        System.out.println(sb);
     }
 }
