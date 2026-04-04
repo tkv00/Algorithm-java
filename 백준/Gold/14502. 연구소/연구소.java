67a58c64e72c572edab1522866ce8208bbc32528
Kim Do Yeon <144890194+tkv00@users.noreply.github.com>
2026-04-04 14:27:45 +0900

    [Gold IV] Title: 연구소, Time: 1412 ms, Memory: 298464 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,33 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Point {
-        int x;
-        int y;
+    private static int N, M;
+    private static StringTokenizer st;
+    private static BufferedReader br;
+    private static final int EMPTY = 0;
+    private static final int WALL = 1;
+    private static final int VIRUS = 2;
+    private static int[][] map;
+    private static int max = 0;
 
-        Point(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+    private static int[] dx = new int[]{0, 0, 1, -1};
+    private static int[] dy = new int[]{1, -1, 0, 0};
+
+    private static boolean isValid(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
     }
 
-    public static int N;
-    public static int M;
-    public static int[][] map;
-    public static StringTokenizer st;
-    public static int[] dx = {1, -1, 0, 0};
-    public static int[] dy = {0, 0, 1, -1};
-    public static Queue<Point> queue;
-    public static int max=0;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static void init() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
         st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
@@ -39,71 +37,96 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-       // System.out.println(Arrays.deepToString(map));
-        dfs(0);
-        System.out.println(max);
 
     }
 
-    //벽 3개 배치
-    public static void dfs(int cnt) {
-        if (cnt == 3) {
-           virus();
-           return;
-        }
+    //바이러스가 퍼지는 시나리오 실행
+    private static int scenario(int[][] map) {
+        boolean[][] visited;
+        Queue<int[]> virus = new LinkedList<>();
+
+
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) {
-                    map[i][j] = 1;
-                    dfs(cnt + 1);
-                    map[i][j] = 0;
+                if (map[i][j] == 2) {
+                    virus.offer(new int[]{i, j});
                 }
             }
         }
+
+        for (int[] now : virus) {
+            Queue<int[]> q = new LinkedList<>();
+            visited = new boolean[N][M];
+            q.offer(new int[]{now[0], now[1]});
+            visited[now[0]][now[1]] = true;
+
+            while (!q.isEmpty()) {
+                int[] next = q.poll();
+
+                for (int d = 0; d < 4; d++) {
+                    int nx = next[0] + dx[d];
+                    int ny = next[1] + dy[d];
+
+                    if (isValid(nx, ny) && !visited[nx][ny] && map[nx][ny] == EMPTY) {
+                        map[nx][ny] = 2;
+                        visited[nx][ny] = true;
+                        q.offer(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+
+        return countSafeZone(map);
     }
 
-    //바이러스 퍼뜨리는 함수
-    public static void virus() {
-        queue = new LinkedList<>();
-        for(int i=0;i<N;i++){
-            for(int j=0;j<M;j++){
-                if(map[i][j]==2){
-                    queue.add(new Point(i,j));
-                }
-            }
+    //바이러스 위치
+    private static void dfs(int wallCount) {
+        if (wallCount == 3) {
+            int[][] newMap = copyMap();
+            max = Math.max(max, scenario(newMap));
+
+            return;
         }
-        int[][] temp = new int[N][M];
+
         for (int i = 0; i < N; i++) {
-            temp[i] = map[i].clone();
-        }
-
-
-        while (!queue.isEmpty()) {
-            Point point = queue.poll();
-            int nowx = point.x;
-            int nowy = point.y;
-            for (int i = 0; i < 4; i++) {
-                int nextX = nowx + dx[i];
-                int nextY = nowy + dy[i];
-                if ((nextX >= 0 && nextX < N) && (nextY >= 0 && nextY < M) && temp[nextX][nextY] == 0) {
-                    temp[nextX][nextY] = 2;
-                    queue.add(new Point(nextX, nextY));
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == EMPTY) {
+                    map[i][j] = WALL;
+                    dfs(wallCount + 1);
+                    map[i][j] = EMPTY;
                 }
             }
         }
-       safezone(temp);
     }
 
-    //안전영력 계산함수
-    public static void safezone(int[][] map) {
+    private static int countSafeZone(int[][] map) {
         int sum = 0;
+
         for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (map[i][j] == 0) {
-                    sum += 1;
-                }
+            for (int j : map[i]) {
+                if (j == 0) sum++;
             }
         }
-       max=Math.max(max,sum);
+
+        return sum;
+    }
+
+    private static int[][] copyMap() {
+        int[][] copy = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                copy[i][j] = map[i][j];
+            }
+        }
+        return copy;
+    }
+
+    public static void main(String[] args) throws IOException {
+        init();
+        dfs(0);
+
+        System.out.print(max);
     }
 }
+
